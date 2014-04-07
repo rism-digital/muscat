45fa4a30b02a2f2f6a3d017f08f38fcacbf5ed54
Laurent Pugin <lxpugin@gmail.com>
2014-04-07 17:08:20 +0200

    updating activeadmin configuration and allowing all fields

--- app/admin/standard_title.rb
+++ app/admin/standard_title.rb
@@ -1,17 +1,13 @@
 ActiveAdmin.register StandardTitle do
 
-  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  # temporarily allow all parameters
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
   
 end
