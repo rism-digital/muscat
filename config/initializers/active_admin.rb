9b31664ef18a54d4cdc08f9bbc48fd010f240936
Rodolfo Zitellini <rwz@xhero.org>
2014-06-02 12:02:48 +0200

    Fixed image lookup, forced JQuery UI css to show active_buttons. Should we get rid of active_button/jquery altogeather?

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -174,7 +174,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-
+  config.register_stylesheet 'jquery.ui.theme.css'
 
   # == CSV options
   #
