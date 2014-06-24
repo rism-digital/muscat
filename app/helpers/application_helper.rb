b1ae9d632a73bbc3da1ceb670bb6bedc9e8a3d66
Rodolfo Zitellini <rwz@xhero.org>
2014-06-24 16:07:10 +0200

    Removed deprecated action_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,51 +70,7 @@ module ApplicationHelper
     it = sprintf("%03d", iterator)
     "#{tag_name}-#{it}-indicator"  
   end
-  
-  # temp. moved here from CW for quick editting
-  def action_button(name, icons, link, other_classes = "", place_icon = :left, corner = :all, id = "", is_function = false)
-    icon = ""
-    classes = ["abutton", "ui-state-default" ]
-    classes << other_classes unless other_classes.empty?
-    if icons.is_a?(String)
-      icon = content_tag(:span, "", {:class => "ui-icon #{icons}"}, false)
-    elsif icons.is_a?(Array)
-      icon = icons.collect {|i| content_tag(:span, "", {:class => "ui-icon #{i}", :style => "float: left;"}, false) }.join("\n")
-    end
-
-    title = ""
-    if name && name.is_a?(Array)
-      title = name[1]
-      name = name[0]
-    end
-
-    if name.nil? or name.empty?
-      label = icon + "_"
-      classes << "abutton-icon-solo"
-    elsif place_icon == :right
-      label = raw(name + icon)
-      classes << "abutton-icon-right"
-    else
-      label = raw(icon + name)
-      classes << "abutton-icon-left"
-    end
-  
-    if corner == :all
-      classes << "ui-corner-all"
-    elsif corner == :left
-      classes << "ui-corner-left"
-    elsif corner == :right
-      classes << "ui-corner-right"
-    end
-
-    if is_function     
-      safe_link_to_function_stub(label, link, { :title => title, :id => id, :class => classes.join(' ') })
-    else
-      link_to(label, link,  { :title => title, :id => id, :class => classes.join(' ') })
-    end
-
-  end
-  
+    
   # This is a safe version of the deprecated link_to_function, left as a transition
   def safe_link_to_function_stub(name, function, html_options={})
     onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;".html_safe
