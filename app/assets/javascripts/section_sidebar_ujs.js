bb401bd224ca18417a20803b2846ea471cb38f0e
Rodolfo Zitellini <rwz@xhero.org>
2014-07-01 10:29:14 +0200

    Show and hode tabs

--- app/assets/javascripts/section_sidebar_ujs.js
+++ app/assets/javascripts/section_sidebar_ujs.js
@@ -1,7 +1,56 @@
 $(document).ready(function(){
 	$('a[data-scroll-target]').click(function(e){
 		e.preventDefault();
+		
+		function show_tab_group(tname) {
+			var current_item = null;
+			var new_item = null;
+		
+			// Hide all the other panels
+			$( ".tab_panel" ).each(function() {
+				if ($(this).attr("name") != tname) {
+				
+					// If we have already an item shown, postpone
+					// the hiding so we can cross fade it
+					if ($(this).data("current-item") == true) {
+						current_item = $(this);
+					} else {
+						$(this).hide();
+					}
+				
+					$(this).removeData("current-item");
+				} else {
+					new_item = $(this);
+					new_item.data("current-item", true);
+				}
+			})
+		
+			// If a group is already visible, cross fade
+			if (current_item) {
+			    current_item.fadeOut('fast', function(){
+			        new_item.fadeIn('fast');
+			    });
+			} else {
+				// No group already there
+				new_item.fadeIn("fast");
+			}
+			//$.scrollTo($("[name=" + tname + "]"), 100, {offset: -10});
+		} // function
+		
+		function show_all_tab_groups() {
+			$( ".tab_panel" ).each(function() {
+				$(this).fadeIn("fast");
+				$(this).removeData("current-item");
+			})
+		} // function
+		
 		tname = $(this).data("scroll-target");
-		$.scrollTo($("[name=" + tname + "]"), 100, {offset: -10});
+		
+		if (tname == "show_all_groups") {
+			show_all_tab_groups();
+		} else {
+			show_tab_group(tname);
+		}
+
 	})
 })
