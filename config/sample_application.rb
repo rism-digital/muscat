24003a59d36cc185a973cd7108a917cec8dd4f0e
Rodolfo Zitellini <rwz@xhero.org>
2014-09-01 09:52:10 +0200

    Fix #15, explictly set config.i18n.enforce_available_locales = true for locale validation

--- config/sample_application.rb
+++ config/sample_application.rb
@@ -29,7 +29,9 @@ module Muscat
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+    
+    # Force validation of locales, this also silences the deprecation warning
+    config.i18n.enforce_available_locales = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
