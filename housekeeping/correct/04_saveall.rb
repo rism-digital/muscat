7f071e991be118f254c91d746d566debb6b2cc7b
Rodolfo Zitellini <rwz@xhero.org>
2014-10-21 15:00:22 +0200

    Script for saving all records, script for comparing marc records

---
+++ housekeeping/correct/04_saveall.rb
@@ -0,0 +1,18 @@
+require 'progress_bar'
+
+pb = ProgressBar.new(Source.all.count)
+
+Source.find_each do |s|
+  
+	pb.increment!
+
+	s.suppress_update_77x
+	s.suppress_update_count
+	s.suppress_reindex
+	begin
+		s.save!
+	rescue => e
+		puts "Could not save source #{s.id}"
+		puts e.exception
+	end
+end
