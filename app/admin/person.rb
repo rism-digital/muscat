c3c0abf534d21b6e1c149f20aa9c2265dc62d5b6
stephan <stephan@hirsch-online.com>
2014-11-26 14:51:32 +0100

    Added submit button to person editor, fixes #80

--- app/admin/person.rb
+++ app/admin/person.rb
@@ -115,6 +115,7 @@ ActiveAdmin.register Person do
   
   sidebar I18n.t(:sections), :only => [:edit, :new] do
     render("editor/section_sidebar") # Calls a partial
+    active_admin_submit_bar( self )
   end
   
   form :partial => "editor/edit_wide"
