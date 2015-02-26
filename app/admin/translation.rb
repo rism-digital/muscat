e1b7391b6f7b38caaf488d135690920aff861648
stephan <stephan@hirsch-online.com>
2015-02-26 15:18:30 +0100

    Improved online translation tool using active-record_store

--- app/admin/translation.rb
+++ app/admin/translation.rb
@@ -1,34 +1,38 @@
 ActiveAdmin.register_page "Translations" do
   menu :parent => "admin_menu", :if => proc{ can? :manage, :all }
-  files=['en.yml', 'de.yml', 'fr.yml', 'it.yml']
 
   page_action :trans, :method=>:post do
-    puts params
     text=params['trans']['body']
     file=session['file']
+    session['content']=text
     schema = Kwalify::Yaml.load_file('config/locales/schemas/lang.schema.yml')
     validator = Kwalify::Validator.new(schema)
     parser = Kwalify::Yaml::Parser.new(validator)
     begin
       parser.parse(text)
-      f=File.open("config/locales/"+file, "w")
+      f=File.open(file, "w")
       f.write(text)
-      redirect_to translations_path, notice: 'Saved'
+      redirect_to translations_path, notice: 'Translation file saved!'
     rescue Kwalify::SyntaxError => e
-      redirect_to translations_path, alert: e.message
+      flash[:error] = "SYNTAX ERROR: "+e.message+" Unable to save translation file!"
+      redirect_to translations_path
     end
   end
 
   page_action :open, :method=>:post do
-    puts params
-    session['file']=params['open_file']['file']
+    file=session['file']=params['open_file']['file']
+    if params['open_file']['content'].size < 2 || file != params['open_file']['recent_file']
+      session['content']=File.open(file, "r").read
+    else
+      session['content']=params['open_file']['content']
+    end
     redirect_to translations_path
   end
 
 
   content do
     panel "Translation" do 
-      render 'index', :files => files
+      render 'index'
     end
   end
 end
