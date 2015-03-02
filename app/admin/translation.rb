3da405d29ac94944bf5f3ea1eff05a17c42e963e
stephan <stephan@hirsch-online.com>
2015-03-02 11:51:31 +0100

    Inserted more yaml validations

--- app/admin/translation.rb
+++ app/admin/translation.rb
@@ -1,34 +1,48 @@
-ActiveAdmin.register_page "Translations" do
+ActiveAdmin.register_page "Translation" do
   menu :parent => "admin_menu", :if => proc{ can? :manage, :all }
 
-  page_action :trans, :method=>:post do
-    text=params['trans']['body']
-    file=session['file']
-    session['content']=text
-    schema = Kwalify::Yaml.load_file('config/locales/schemas/lang.schema.yml')
-    validator = Kwalify::Validator.new(schema)
-    parser = Kwalify::Yaml::Parser.new(validator)
-    begin
-      parser.parse(text)
-      f=File.open(file, "w")
-      f.write(text)
-      redirect_to translations_path, notice: 'Translation file saved!'
-    rescue Kwalify::SyntaxError => e
-      flash[:error] = "SYNTAX ERROR: "+e.message+" Unable to save translation file!"
-      redirect_to translations_path
+  page_action :edit, :method=>:post do
+    if params['act']['Open']
+      file=session['file']=params['open_file']['file']
+      if params['open_file']['content'].size < 2 || file != params['open_file']['recent_file']
+        session['content']=File.open(file, "r").read
+      else
+        session['content']=params['open_file']['content']
+      end
+      redirect_to translation_path
+    end
+    if params['act']['Save']
+      text=params['open_file']['content']
+      file=session['file']
+      session['content']=text
+      schema = Kwalify::Yaml.load_file('config/locales/schemas/lang.schema.yml')
+      validator = Kwalify::Validator.new(schema)
+      begin
+        document = Kwalify::Yaml::load(text)
+        errors=validator.validate(document)
+        puts errors
+        puts errors.size
+        ## show errors
+        if errors && !errors.empty?
+          for e in errors
+            if e.message.include?("required")
+
+              flash[:error] = "SYNTAX ERROR: "+e.message+" Unable to save translation file!"
+              redirect_to translation_path
+              return 1
+            end
+            puts "[#{e.path}] #{e.message}"
+          end
+        end
+        f=File.open(file, "w")
+        f.write(text)
+        redirect_to translation_path, notice: 'Translation file saved!'
+      rescue Kwalify::SyntaxError => e
+        flash[:error] = "SYNTAX ERROR: "+e.message+" Unable to save translation file!"
+        redirect_to translation_path
+      end
     end
   end
-
-  page_action :open, :method=>:post do
-    file=session['file']=params['open_file']['file']
-    if params['open_file']['content'].size < 2 || file != params['open_file']['recent_file']
-      session['content']=File.open(file, "r").read
-    else
-      session['content']=params['open_file']['content']
-    end
-    redirect_to translations_path
-  end
-
 
   content do
     panel "Translation" do 
