47d76d47a62152aa84cafe8c4fb580af86347a1c
Rodolfo Zitellini <rwz@xhero.org>
2015-07-16 14:28:09 +0200

    Fix invalid model check AA evaluates content do at load time so @model there is always nil.

--- app/admin/doc.rb
+++ app/admin/doc.rb
@@ -4,17 +4,12 @@ ActiveAdmin.register_page "doc" do
     def index
       @model_name = params[:model].downcase
       klass = params[:model].classify.safe_constantize
-      
+
       @model = klass != nil ? klass.new : nil
-      
     end
   end
   
   content do
-    if @model != nil
-      render partial: 'fields'
-    else
-      para "Please specify a valid model."
-    end
+    render partial: 'fields'
   end
 end
