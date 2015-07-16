3dabb8ebcc08bcb3dde623bf23252c923b1837ae
Laurent Pugin <lxpugin@gmail.com>
2015-07-16 15:25:20 +0200

    More marc doc in AA

--- app/admin/doc.rb
+++ app/admin/doc.rb
@@ -1,15 +1,25 @@
 ActiveAdmin.register_page "doc" do
+    menu :parent => "admin_menu", :label => proc {I18n.t(:menu_marc_documentation)}, :if => proc{ can? :manage, User }
   
   controller do
     def index
+      params[:model] = "Source" if !params[:model]
       @model_name = params[:model].downcase
       klass = params[:model].classify.safe_constantize
-
       @model = klass != nil ? klass.new : nil
     end
   end
   
-  content do
+  content title: proc{ I18n.t(:menu_marc_documentation) + " - " + @model.class.name } do
     render partial: 'fields'
   end
+  
+  ###########
+  ## Index ##
+  ###########
+  
+  sidebar :models, :class => "sidebar_tabs", :only => [:index] do
+    render("doc_sidebar") # Calls a partial
+  end
+  
 end
