e7cc63ad317659194ca14e2fba0920a6168f6dc4
Rodolfo Zitellini <rwz@xhero.org>
2015-07-28 14:01:20 +0200

    Script for substituting $3 with $8

---
+++ housekeeping/upgrade_3.2/001_migrate_marc.rb
@@ -0,0 +1,50 @@
+# these fields come from marc conf
+# alternatively we can search for $3 in all fields and signal
+# unconfigured ones?
+fields3 = ["260", "300", "340", "351", "590", "592", "593"]
+
+Source.all.each do |s|
+  
+  begin
+    marc = s.marc
+    x = marc.to_marc
+  rescue => e
+    puts e.exception
+    next
+  end
+  
+  modified = false
+  fields_mod = []
+  
+  fields3.each do |field|
+  
+    marc.each_by_tag(field) do |t|
+    
+      a = t.fetch_all_by_tag("3")
+      next if !a 
+
+      subtag_changed = false
+
+      a.each do |subtag|
+        next if !subtag && !subtag.content
+        
+        t.add_at(MarcNode.new(Source, "8", subtag.content, nil), 0)
+        t.destroy_child(subtag)
+        subtag_changed = true
+      end
+    
+      if subtag_changed
+        t.sort_alphabetically
+      
+        modified = true
+        fields_mod << field
+      end
+    end
+  end
+  
+  if modified
+    puts "Saving #{s.id}, fields #{fields_mod.to_s}"
+    s.save!
+  end
+  
+end
