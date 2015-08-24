e7d602c49ac2f1373491801bbc45337bb9f09a37
Rodolfo Zitellini <rwz@xhero.org>
2015-08-24 14:35:10 +0200

    Add lock_version to all models

--- db/migrate/20150819094054_add_locking_support.rb
+++ db/migrate/20150819094054_add_locking_support.rb
@@ -1,5 +1,5 @@
 class AddLockingSupport < ActiveRecord::Migration
-  def change
+  def change    
     unless column_exists? :people, :lock_version
       add_column :people, :lock_version, :integer, { :default => 0, :null => false }
     end
