9ee246167a64bc95d56d9c5f10a35e899c52b32f
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-09-04 18:33:13 +0200

    Tabs to spaces in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Adds a few additional behaviors into the application controller 
+  # Adds a few additional behaviors into the application controller
    include Blacklight::Controller
-  # Please be sure to impelement current_user and user_session. Blacklight depends on 
-  # these methods in order to perform user specific actions. 
+  # Please be sure to impelement current_user and user_session. Blacklight depends on
+  # these methods in order to perform user specific actions.
 
   layout 'blacklight'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_filter :set_locale
 
   after_filter :user_activity
-  
+
   # Code for rescueing lock conflicts errors
   rescue_from ActiveRecord::StaleObjectError do |exception|
      respond_to do |format|
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
        format.json { head :conflict }
     end
   end
-  
+
   def user_for_paper_trail
     current_user.try :name
   end
@@ -35,11 +35,11 @@ class ApplicationController < ActionController::Base
   def user_activity
       current_user.try :touch
   end
-  
+
   # Find out and set the locale, store into a cookie
-  def set_locale 
+  def set_locale
     # We do not check if the locale is available. The list is actually set in the
-    # menu (see active_admin.rb) 
+    # menu (see active_admin.rb)
     if params[:locale] ## && AVAILABLE_LOCALES.include?(params[:locale])
     # user is changing locale, keep it for the session and in a cookie
       session[:locale] = params[:locale]
@@ -47,32 +47,32 @@ class ApplicationController < ActionController::Base
     elsif !session[:locale]
       # no locale for the session yet, use cookie or http_header (or default)
       if (cookies[:locale]) # && AVAILABLE_LOCALES.include?(cookies[:locale])
-        session[:locale] = cookies[:locale] 
+        session[:locale] = cookies[:locale]
       else
-        #logger.debug "HTTP_ACCEPT_LANGUAGE:#{request.env['HTTP_ACCEPT_LANGUAGE']}" 
+        #logger.debug "HTTP_ACCEPT_LANGUAGE:#{request.env['HTTP_ACCEPT_LANGUAGE']}"
         session[:locale] = _locale_from_http_header
         cookies[:locale] = { :value => session[:locale], :expires => 30.days.from_now }
       end
-    end   
+    end
     #logger.debug "LOCALE", I18n.locale
     I18n.locale = session[:locale]
-  end 
-  
-  def restore_search_filters  
   end
-  
-  def save_search_filters  
+
+  def restore_search_filters
   end
-  
+
+  def save_search_filters
+  end
+
   private
-  
+
   # Parse the http header to get a locale
   # Hard-coded list - to be improved
-  def _locale_from_http_header 
+  def _locale_from_http_header
     return "en" if !request.env || !request.env.include?('HTTP_ACCEPT_LANGUAGE')
-    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first 
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     return locale if ["en", "fr", "it", "de"].include?(locale)
     "en"
-  end 
+  end
 
 end
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 #
-class CatalogController < ApplicationController  
-  
+class CatalogController < ApplicationController
+
   before_action :redirect_legacy_values, :only => :show
-  
+
   def redirect_legacy_values
     # Rewrite old IDS with five leading zeros
     if params[:id].start_with?('00000')
@@ -15,7 +15,7 @@ class CatalogController < ApplicationController
   def mei
     @item = Source.find(params[:id])
   end
-  
+
   def download_xslt
     send_file(
       "#{Rails.root}/public/xml/marc2mei.xsl",
@@ -23,12 +23,12 @@ class CatalogController < ApplicationController
       type: "text/xsl"
     )
   end
-  
+
   include Blacklight::Catalog
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
-    config.default_solr_params = { 
+    config.default_solr_params = {
       :qt => 'search',
       :"q.alt" => "*:*",
       :rows => 20,
@@ -39,15 +39,15 @@ class CatalogController < ApplicationController
       :"hl.simple.post" => "</span>",
       :"facet.mincount" => 1,
     }
-    
+
     # solr path which will be added to solr base url before the other solr params.
-    #config.solr_path = 'select' 
-    
+    #config.solr_path = 'select'
+
     # items to show per page, each number in the array represent another option to choose from.
     config.per_page = [10,20,50,100]
     config.default_per_page = 20
 
-    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
     #
     config.default_document_solr_params = {
@@ -83,17 +83,17 @@ class CatalogController < ApplicationController
     # * If left unset, then all facet values returned by solr will be displayed.
     # * If set to an integer, then "f.somefield.facet.limit" will be added to
     # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _displayed_ in a page.    
+    # you configure the number of items you actually want _displayed_ in a page.
     # * If set to 'true', then no additional parameters will be sent to solr,
     # but any 'sniffed' request limit parameters will be used for paging, with
-    # paging at requested limit -1. Can sniff from facet.limit or 
+    # paging at requested limit -1. Can sniff from facet.limit or
     # f.specific_field.facet.limit solr request params. This 'true' config
     # can be used if you set limits in :default_solr_params, or as defaults
     # on the solr side in the request handler itself. Request handler defaults
     # sniffing requires solr requests to be made with "echoParams=all", for
-    # app code to actually have it echo'd back to see it.  
+    # app code to actually have it echo'd back to see it.
     #
-    # :show may be set to false if you don't want the facet to be drawn in the 
+    # :show may be set to false if you don't want the facet to be drawn in the
     # facet bar
     config.add_facet_field 'std_title_order_s', :label => :filter_std_title, :limit => 10
     config.add_facet_field 'composer_order_s', :label => :filter_composer, :limit => 10
@@ -104,11 +104,11 @@ class CatalogController < ApplicationController
     config.add_facet_field 'lib_siglum_order_s', :label => :filter_lib_siglum, :limit => 10
     config.add_facet_field '650a_filter_sm', :label => :filter_subject, :limit => 10
     #config.add_facet_field 'title_order', :label => 'Standard Title', :single => true
-    #config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    #config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    #config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    #config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    #config.add_facet_field 'subject_era_facet', :label => 'Era'  
+    #config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20
+    #config.add_facet_field 'language_facet', :label => 'Language', :limit => true
+    #config.add_facet_field 'lc_1letter_facet', :label => 'Call Number'
+    #config.add_facet_field 'subject_geo_facet', :label => 'Region'
+    #config.add_facet_field 'subject_era_facet', :label => 'Era'
 
     #config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
 
@@ -129,7 +129,7 @@ class CatalogController < ApplicationController
     # configured above
 
     # solr fields to be displayed in the show (single result) view
-    #   The ordering of the field names is the order of the display 
+    #   The ordering of the field names is the order of the display
     #config.add_show_field 'title_vern_display', :label => 'Title'
     #config.add_show_field 'subtitle_display', :label => 'Subtitle'
     #config.add_show_field 'subtitle_vern_display', :label => 'Subtitle'
@@ -156,68 +156,68 @@ class CatalogController < ApplicationController
     # The :key is what will be used to identify this BL search field internally,
     # as well as in URLs -- so changing it after deployment may break bookmarked
     # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different. 
+    # or can be specified manually to be different.
 
     # This one uses all the defaults set by the solr request handler. Which
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise. 
-    
+    # since we aren't specifying it otherwise.
+
     #config.add_search_field('author') do |field|
     #  field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-    #  field.solr_local_parameters = { 
+    #  field.solr_local_parameters = {
     #    :qf => '$author_qf',
     #    :pf => '$author_pf'
     #  }
     #end
-    
+
     # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as 
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
+    # tests can test it. In this case it's the same as
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     #config.add_search_field('subject') do |field|
     #  field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
     #  field.qt = 'search'
-    #  field.solr_local_parameters = { 
+    #  field.solr_local_parameters = {
     #    :qf => '$subject_qf',
     #    :pf => '$subject_pf'
     #  }
     #end
-    
+
     config.add_search_field('any_field') do |field|
       field.label = :filter_any_field
       # Dumb was here
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         # THIS IS THE HACK OF THE DAY
         #FIXME FIXME FIXME
         :qf => 'id_fulltext_text source_id_text std_title_texts std_title_d_text composer_texts composer_d_text title_texts title_d_text shelf_mark_texts lib_siglum_texts 001_text 008_date1_text 008_date2_text 008_language_text 028a_text 028b_text 031d_text 031e_text 031m_text 031p_texts 031q_text 031t_text 033a_text 035a_text 041a_text 041e_text 041h_text 100a_text 100d_text 110a_text 110b_text 130k_text 130m_text 130n_texts 130o_text 130p_text 130r_text 240k_text 240m_texts 240n_texts 240o_text 240p_text 240r_texts 245a_text 245b_text 245c_text 246a_text 246i_text 254a_text 260a_text 260b_text 260c_text 260e_text 260f_text 270a_text 300a_text 300b_text 300c_text 340c_text 340d_text 351a_text 500a_text 5005_text 505a_text 506a_text 508a_text 510a_text 510c_text 511a_text 518a_text 520a_text 525a_text 533a_text 541a_text 541c_text 541d_text 545a_text 546a_text 555a_text 561a_text 5615_text 562a_text 5625_text 563a_text 5635_text 590a_text 590b_text 591a_text 592a_text 593a_texts 594a_text 594b_text 594c_text 594d_text 594e_text 594f_text 594g_text 594h_text 594i_text 594k_text 594l_text 594m_text 594n_text 595a_text 596a_text 597a_text 598a_text 599a_text 600a_texts 650a_text 651a_text 653a_text 657a_text 690a_text 690n_text 691a_text 700a_text 700d_text 700e_text 700t_text 7004_text 7005_text 710a_text 710b_text 710k_text 710e_text 7104_text 7105_text 730a_text 730k_text 730m_text 730n_text 730o_text 730p_text 730r_text 740a_text 752a_text 752d_text 772w_text 773w_text 786a_text 786d_text 786i_text 786o_text 786t_text 787a_text 787n_text 787w_text 852a_text 852b_text 852d_text 852e_text 852p_texts 852q_text 852z_text 856u_text'
       }
     end
-    
+
     config.add_search_field('composer') do |field|
       field.label = :filter_composer
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => 'composer_texts',
       }
     end
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields. 
-    
+    # of Solr search fields.
+
     config.add_search_field('title_all') do |field|
       field.label = :filter_title
       field.include_in_advanced_search = false # This is only for the topbar
-      # solr_parameters hash are sent to Solr as ordinary url query params. 
+      # solr_parameters hash are sent to Solr as ordinary url query params.
       #field.solr_parameters = { :'spellcheck.dictionary' => 'title_d_text' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => 'title_texts std_title_texts',
       }
     end
-    
+
     # Add some filters for the adv search
     config.add_search_field("title") do |field|
       field.label = :filter_title_on_ms
@@ -231,67 +231,67 @@ class CatalogController < ApplicationController
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "std_title_texts" }
     end
-    
+
     config.add_search_field("provenance") do |field|
       field.label = :filter_provenance
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "561a_text" }
     end
-    
+
     config.add_search_field("source_type") do |field|
       field.label = :filter_source_type
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "593a_texts" }
     end
-    
+
     config.add_search_field("liturgical_feast") do |field|
       field.label = :filter_liturgical_feast
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "657a_text" }
     end
-    
+
     config.add_search_field("institution") do |field|
       field.label = :filter_institution
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "110a_test" }
     end
-    
+
     config.add_search_field("catalogue") do |field|
       field.label = :filter_catalog
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "690a_text" }
     end
-    
+
     config.add_search_field("scoring") do |field|
       field.label = :filter_scoring
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "240m_texts" }
     end
-    
+
     # This is shown in the topbar
     config.add_search_field("library_siglum") do |field|
       field.label = :filter_lib_siglum
       field.solr_parameters = { :qf => "852a_text" }
     end
-    
+
     config.add_search_field("rism_id_no") do |field|
       field.label = :filter_id
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "id_i" }
     end
-    
+
     config.add_search_field("year") do |field|
       field.label = :filter_date
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "date_from_i" }
     end
-    
+
     config.add_search_field("shelfmark") do |field|
       field.label = :filter_shelf_mark
       field.include_in_simple_select = false
       field.solr_parameters = { :qf => "852p_texts" }
     end
-    
+
     config.add_search_field("language") do |field|
       field.label = :filter_language
       field.include_in_simple_select = false
@@ -316,9 +316,9 @@ class CatalogController < ApplicationController
     #config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
     #config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
 
-    # If there are more than this many search results, no spelling ("did you 
+    # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
   end
 
-end 
+end
