06c953255b4efe5b4ce1abacb445e534b509ecde
Rodolfo Zitellini <rwz@xhero.org>
2015-11-27 12:35:31 +0100

    Suppress relation creation in source import

--- lib/marc_import.rb
+++ lib/marc_import.rb
@@ -12,8 +12,8 @@ class MarcImport
     @model = model
     @total_records = open(source_file) { |f| f.grep(/001">/) }.size
     @import_results = Array.new
-    @cnt=0
-    @start_time=Time.now
+    @cnt = 0
+    @start_time = Time.now
   end
 
   #Helper method to parse huge files with nokogiri
@@ -30,42 +30,40 @@ class MarcImport
   def import
     #line_number = 0
     each_record(@source_file) { |record|         
-        rec=Nokogiri::XML(record.to_s)
-
+        rec = Nokogiri::XML(record.to_s)
         # Use external XSLT 1.0 file for converting to MARC21 text
-        xslt  = Nokogiri::XSLT(File.read(Rails.root.join('housekeeping/import/', 'marcxml2marctxt_record.xsl')))
-        marctext=xslt.transform(rec).to_s
+        xslt  = Nokogiri::XSLT(File.read(Rails.root.join('housekeeping/import/', 'marcxml2marctxt_1.0.xsl')))
+        marctext = xslt.transform(rec).to_s
         create_record(marctext)
     }
     puts @import_results
   end
 
-  def create_record(buffer, line_number=0)
-    @cnt+=1
+  def create_record(buffer, line_number = 0)
+    @cnt += 1
     #@total_records += 1
     buffer.gsub!(/[\r\n]+/, ' ')
     buffer.gsub!(/ (=[0-9]{3,3})/, "\n\\1")
     if @total_records >= @from
-      marc = Object.const_get("Marc"+@model).new(buffer)
+      marc = Object.const_get("Marc" + @model).new(buffer)
       # load the text source but without resolving externals
       marc.load_source(false)
       if marc.is_valid?(false)
-        #p marc.get_marc_source_id
+       
         # step 1.  update or create a new object
-        model = Object.const_get(@model).find_by_id( marc.get_id )
+        model = Object.const_get(@model).find_by_id(marc.get_id)
         if !model
-          status="created"
-          if @model=="Catalogue"
+          status = "created"
+          if @model == "Catalogue"
             model = Object.const_get(@model).new(:id => marc.get_id, :name => marc.get_name, :author => marc.get_author, :revue_title=> marc.get_revue_title, :description => marc.get_description, :wf_owner => 1, :wf_stage => "published", :wf_audit => "approved")
 
-          elsif @model=="Person" || @model =="Institution"
+          elsif @model == "Person" || @model == "Institution"
             model = Object.const_get(@model).new(:id => marc.get_id, :wf_owner => 1, :wf_stage => "published", :wf_audit => "approved")
-          elsif @model=="Source"
+          elsif @model == "Source"
             model = Object.const_get(@model).new(:id => marc.get_id, :lib_siglum => marc.get_siglum, :wf_owner => 1, :wf_stage => "published", :wf_audit => "approved")
           end
         else
-          status="updated"
-
+          status = "updated"
         end
         #p model
           
@@ -76,11 +74,15 @@ class MarcImport
         model.marc = marc
         @import_results.concat( marc.results )
         @import_results = @import_results.uniq
-        # step 4. insert model into database
-        #model.suppress_update_77x # we should not need to update the 772/773 relationships during the import
-        #model.suppress_create_incipit
-        #model.suppress_create_incipit
+
+        if if @model == "Source"
+          source.suppress_update_77x # we should not need to update the 772/773 relationships during the import
+          source.suppress_update_count # Do not update the count for the foreign objects
+        end
+        
         model.suppress_reindex
+        
+         # step 4. insert model into database
         begin
           model.save #
           @log.info(@model+" record "+marc.get_id.to_s+" "+status)
@@ -93,7 +95,7 @@ class MarcImport
           #puts marc.get_id.to_s
           puts e.class
         end
-        print "\rStarted: "+@start_time.strftime("%Y-%m-%d %H:%M:%S")+" -- Record #{@cnt} of #{@total_records} processed"
+        print "\rStarted: " + @start_time.strftime("%Y-%m-%d %H:%M:%S") + " -- Record #{@cnt} of #{@total_records} processed"
         #puts "Last offset: #{@total_records}, Last "+@model+" RISM ID: #{marc.first_occurance('001').content}"
       else
         puts "failed to import marc record leading up to line #{line_number}"
