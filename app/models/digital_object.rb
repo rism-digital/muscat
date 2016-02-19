1d3142346ea719e96bcdcd877b82601f5feb9251
Laurent Pugin <lxpugin@gmail.com>
2016-02-19 11:53:52 +0100

    Making path configurable

--- app/models/digital_object.rb
+++ app/models/digital_object.rb
@@ -1,10 +1,14 @@
 class DigitalObject < ActiveRecord::Base
   
-    # atttachments
-    has_attached_file :attachment, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/attachment/missing.png"
+    # attachments
+    has_attached_file :attachment, 
+      styles: { maximum: "900x900", medium: "300x300", thumb: "100x100>" }, 
+      default_url: "/images/attachment/missing.png",
+      path: "#{RISM::DIGITAL_OBJECT_PATH}/system/:class/:attachment/:id_partition/:style/:filename"
     validates_attachment :attachment, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   
     belongs_to :source
     has_many :folder_items, :as => :item
     belongs_to :user, :foreign_key => "wf_owner"
+
 end
