692bb98cde099705550fa88e07808f88378f9fec
stephan <stephan@hirsch-online.com>
2016-02-25 17:33:32 +0100

    Added some form inject methods

--- app/assets/javascripts/viaf_interface.js
+++ app/assets/javascripts/viaf_interface.js
@@ -5,28 +5,23 @@ var show_viaf_actions = function () {
 		marc_editor_show_panel("viaf-form");
 	});
 
-
-	//needs improvement!
 	$viaf_table.delegate('.data', 'click', function() {
 		_update_form($(this).data("viaf"));
-		var elem = $(".tag_container[data-tag='024']");
-		field024_2 = elem.find("select.subfield_entry[data-tag='024'][data-subfield='2']")
-		field024_2.first().val("VIAF");
+		marc_editor_show_panel("marc_editor_panel");
 	});
 
 	function _update_form(data){
-		marc_json = data;
-		id = marc_json_get_tags(marc_json, "001")[0].content
-		var elem = $(".tag_container[data-tag='024']");
-		var collapse = elem.children(".tag_content_collapsable");
-		field024_a = elem.find("input.subfield_entry[data-tag='024'][data-subfield='a']").first()
-		field024_a[0].value = id;
-		field024_a.css("background-color", "#ffffb3");
-		tag_header_add_from_empty(elem);
-		marc_editor_show_panel("marc_editor_panel");		
-			}
-
-
+		id = marc_json_get_tags(data, "001")[0]
+		if (_size_of_marc_tag("024") == 0){
+			_new_marc_tag("024", id);
+		}
+		else{
+			_append_marc_tag("024", id);
+		}
+		//update only if new record; needs improvement
+		//check if new record
+		_edit_marc_tag("100", marc_json_get_tags(data, "100")[0])
+	}
 
 	$("#viaf_button").click(function(){
 		$viaf_table.html("");
@@ -62,9 +57,67 @@ var show_viaf_actions = function () {
 		row.append($("<td>" + tag100["a"] + "</td>"));
 		row.append($("<td>" + (tag100["d"] ? tag100["d"] : "") + "</td>"));
 		row.append($("<td>" + tag100["0"] + "</td>"));
-		row.append($('<td><a class="data" data-viaf=\'' + JSON.stringify(rowData) + '\'>Übernehmen</a></td>'));
+		row.append($('<td><a class="data" href="#" data-viaf=\'' + JSON.stringify(rowData) + '\'>Übernehmen</a></td>'));
 	}
 };
+
+function _new_marc_tag(target, data) {
+	field = $(".tag_placeholders[data-tag='"+ target +"']")
+	group = field.parents(".tag_group");
+	block = group.children(".marc_editor_tag_block");
+	new_dt = field.children(".tag_container").clone();
+	subfield = new_dt.find("input.subfield_entry[data-tag='" + target + "'][data-subfield='a']").first()
+	subfield.val(data.content);
+	if(data.tag == "001"){
+		provider = new_dt.find("select.subfield_entry[data-tag='" + target + "'][data-subfield='2']").first()
+		provider.val("VIAF");
+	}
+	subfield.css("background-color", "#ffffb3");
+	new_dt.toggleClass('tag_placeholders tag_toplevel_container');
+	block.append(new_dt);
+	_update_empty_marc_tag(group);
+}
+
+function _edit_marc_tag(target, data) {
+	block = $(".marc_editor_tag_block[data-tag='" + target + "']")
+	field = block.find("input.subfield_entry[data-tag='" + target + "'][data-subfield='a']").first()
+	field.val(data["a"]);
+	field.css("background-color", "#ffffb3");
+}
+
+
+function _append_marc_tag(target, data) {
+	block = $(".marc_editor_tag_block[data-tag='" + target + "']")
+	placeholder = block.parents(".tag_group").children(".tag_placeholders_toplevel").children(".tag_placeholders");
+	new_dt = placeholder.clone()
+	subfield = new_dt.find("input.subfield_entry[data-tag='" + target + "'][data-subfield='a']").first()
+	subfield.val(data.content);
+	if(data.tag == "001"){
+		provider = new_dt.find("select.subfield_entry[data-tag='" + target + "'][data-subfield='2']").first()
+		provider.val("VIAF");
+	}
+	subfield.css("background-color", "#ffffb3");
+	new_dt.toggleClass('tag_placeholders tag_toplevel_container');
+	block.append(new_dt)
+	new_dt.show()
+}
+
+
+function _size_of_marc_tag(tag){
+	fields = $(".tag_toplevel_container[data-tag='"+ tag +"']")
+	return fields.size()		
+}
+
+
+function _update_empty_marc_tag(tag_group) {
+	if ( tag_group.children(".marc_editor_tag_block").children().size() > 0 ) {
+		tag_group.children(".tag_empty_container").hide();
+	}
+	else {
+		tag_group.children(".tag_empty_container").show();
+	}
+}
+
 
 $(document).ready(show_viaf_actions);
 
