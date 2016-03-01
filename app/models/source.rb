f640faef1f2fce356ae145d1e8ab3a402adee803
Rodolfo Zitellini <rwz@xhero.org>
2016-03-01 13:07:31 +0100

    Special method to delete invalid sources

--- app/models/source.rb
+++ app/models/source.rb
@@ -71,7 +71,7 @@ class Source < ActiveRecord::Base
   before_save :set_object_fields
   after_create :fix_ids
   after_save :update_links, :reindex
-  before_destroy :update_links
+  before_destroy :update_links_for_destroy
   
   # alias for holding records
   alias_attribute :ms_condition, :title  
@@ -126,6 +126,15 @@ class Source < ActiveRecord::Base
     
     # update the parent manuscript when having 773/772 relationships
     update_77x unless self.suppress_update_77x_trigger == true 
+  end
+  
+  # A special case: if we are deleting the source
+  # do not update the 77x links. This permits
+  # us to delete sources that have invalid MARC data
+  # since 77x forces a marc load
+  def update_links_for_destroy
+    suppress_update_77x
+    update_links
   end
   
   # Suppresses the solr reindex
