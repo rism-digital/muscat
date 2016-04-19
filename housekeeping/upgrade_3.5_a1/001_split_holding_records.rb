337dd535def16c480e8c7acbda0d220164c170a2
Rodolfo Zitellini <rwz@xhero.org>
2016-04-19 14:39:12 +0200

    Fix deletion problem in holding creation script

--- housekeeping/upgrade_3.5_a1/001_split_holding_records.rb
+++ housekeeping/upgrade_3.5_a1/001_split_holding_records.rb
@@ -5,10 +5,12 @@ Source.all.each do |source|
   marc = source.marc
   marc.load_source
   
+  count = 0
+  
   marc.each_by_tag("852") do |t|
     
     # Make a nice new holding record
-    holding = Holding.new    
+    holding = Holding.new
     new_marc = MarcHolding.new(File.read("#{Rails.root}/config/marc/#{RISM::MARC}/holding/default.marc"))
     new_marc.load_source false
     
@@ -25,9 +27,25 @@ Source.all.each do |source|
     holding.source = source
     holding.save
     
-    t.destroy_yourself #adios
+    count += 1
+  end
+
+  if count != source.holdings.count
+    puts "OH NOOOO"
+    ENDMENOW
   end
   
+  ts = marc.root.fetch_all_by_tag("852") 
+  ts.each {|t2| t2.destroy_yourself}
+  
+	source.suppress_update_77x
+	source.suppress_update_count
+  source.suppress_reindex
+  
+  new_marc_txt = marc.to_marc
+  new_marc = MarcSource.new(new_marc_txt, source.record_type)
+  source.marc = new_marc
+
   source.save
   
   pb.increment!
