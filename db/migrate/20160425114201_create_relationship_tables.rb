d278ceaa29b7f6de6bda74658d71e81d162d206e
Rodolfo Zitellini <rwz@xhero.org>
2016-04-25 15:33:02 +0200

    Fix relation creation

--- db/migrate/20160425114201_create_relationship_tables.rb
+++ db/migrate/20160425114201_create_relationship_tables.rb
@@ -1,14 +1,5 @@
 class CreateRelationshipTables < ActiveRecord::Migration
   def self.up
-
-    create_table(:people_to_institutions, :id => false) do |t|
-      t.column :person_id, :integer
-      t.column :institution_id, :integer 
-    end
-    
-    add_index :people_to_institutions, :person_id
-    add_index :people_to_institutions, :institution_id
-    
     create_table(:institutions_to_people, :id => false) do |t|
       t.column :institution_id, :integer 
       t.column :person_id, :integer
