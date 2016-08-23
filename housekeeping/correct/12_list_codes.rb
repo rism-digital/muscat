d6a1fca698098dea5b2e73ab0ff2c3c7834a2120
Rodolfo Zitellini <rwz@xhero.org>
2016-08-23 09:16:50 +0200

    Better tag listing script

--- housekeeping/correct/12_list_codes.rb
+++ housekeeping/correct/12_list_codes.rb
@@ -1,24 +1,32 @@
 require 'progress_bar'
 pb = ProgressBar.new(Source.all.count)
 
+@editor_profile = EditorConfiguration.get_default_layout Source.first
+
 codes = {}
 Source.find_in_batches do |batch|
 
-	batch.each do |s|
-		pb.increment!
-		s.marc.each_by_tag("700") do |t|
-			tn = t.fetch_first_by_tag("4")
+       	batch.each do |s|
+       		pb.increment!
+       		s.marc.each_by_tag("710") do |t|
+       			tn = t.fetch_first_by_tag("4")
 
-			next if !(tn && tn.content)
+       			next if !(tn && tn.content)
 
-			if codes.include?(tn.content)
-				codes[tn.content] += 1
-			else
-				codes[tn.content] = 1
-			end
-		end
-	end
-	#ap codes
+       			if codes.include?(tn.content)
+       				codes[tn.content] += 1
+       			else
+       				codes[tn.content] = 1
+       			end
+       		end
+       	end
+       	#ap codes
 end
 
-ap codes
+codes_sorted =  codes.sort_by{|k,v| v}
+
+codes_sorted.each do |a|
+       	k = a[0]
+       	c = a[1]
+       	puts @editor_profile.get_label(k.to_s) + " #{k} #{c}"
+end
