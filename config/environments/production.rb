d5d0a91a02c7ededf4033f0f092aaaa8e8774125
Rodolfo Zitellini <rwz@xhero.org>
2016-11-08 09:49:28 +0100

    Add exception notifier for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,9 @@ Muscat::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
