0e242b188bd9c0aeb0837e46e28dbb859796e04c
Rodolfo Zitellini <rwz@xhero.org>
2016-11-08 11:19:13 +0100

    Reindex script. Rename serve_static_assets to serve_static_files as per warning.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Muscat::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -80,5 +80,5 @@ Muscat::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.serve_static_assets = true
+  config.serve_static_files = true
 end
