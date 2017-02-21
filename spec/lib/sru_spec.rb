cec339b87d2675689835cf707731c531b8acc472
stephan <stephan@hirsch-online.com>
2017-02-21 17:11:35 +0100

    Fixed wrong array in explain partial

--- spec/lib/sru_spec.rb
+++ spec/lib/sru_spec.rb
@@ -89,29 +89,4 @@ describe Sru::Query do
       end
     end
   end
-
-  describe SruController, :type => :controller do
-    render_views
-    context "Default action should return the explain page"
-      it "returns the explain page" do
-        get "service"
-        doc = Nokogiri::XML(response.body)
-        namespace="http://explain.z3950.org/dtd/2.0/" 
-        hostinfo = doc.xpath("//ns:serverInfo/ns:host", "ns" => namespace).first.content
-        expect(hostinfo).to be == "rism.info"
-      end
-  end
-
-  describe SruController, :type => :controller do
-    render_views
-    context "MaximumRecords should be limited to 100"
-      it "returns the error page with limitiation" do
-        get "service", {:query => 'Bach', :operation => "searchRetrieve", :maximumRecords => 200}
-        doc = Nokogiri::XML(response.body)
-        diag = "http://www.loc.gov/zing/srw/diagnostic/"
-        max = doc.xpath("//diag:message", "diag" => diag).first.content
-        expect(max).to match(/MaximumRecords is limited/)
-      end
-  end
-
 end
