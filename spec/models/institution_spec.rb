480fc80e38dd7d1f8fd5bfe836e25cf459d61188
stephan <stephan@hirsch-online.com>
2017-03-08 10:42:58 +0100

    Fixes #486 false workgroup trigger in institution

---
+++ spec/models/institution_spec.rb
@@ -0,0 +1,15 @@
+describe Institution do
+
+  describe "#update_workgroups" do
+    context "update workgroups should trigger update workgroups after creating new institution with siglum" do
+      before_size = Workgroup.where(:name => 'Germany').take.institutions.size
+      institution = Institution.create(:name => "Entenhausen", :siglum => "D-Enteh")
+      after_size = Workgroup.where(:name => 'Germany').take.institutions.size
+      it "compares the sizes of depending workgroups" do
+        expect((before_size + 1)).to be == after_size
+      end
+      institution.destroy
+    end
+  end
+end
+ 
