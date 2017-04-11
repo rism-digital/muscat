7ccbf1fbf3e2774b13be3475c8c56477945c725d
Rodolfo Zitellini <rwz@xhero.org>
2017-04-11 11:25:46 +0200

    Do some magic finding 033s. Included as a reference if needed

---
+++ housekeeping/correct/21_033.rb
@@ -0,0 +1,77 @@
+Source.find_in_batches do |batch|
+  batch.each do |s|
+
+    s.marc.load_source false
+    # IF none we do not care
+    next if s.marc.by_tags("033").count == 0
+  
+    #puts "#{s.id}\t\t"
+    
+    all_dates = []
+    
+    s.marc.each_by_tag("033") do |t|
+      
+      t.each_by_tag("a") do |t|
+        dates033 = []
+        next if !t || !t.content
+        
+        # 033s can be like "12345678, 12345678)"
+        toks = t.content.split(",")
+        toks.each do |date_part|
+          dates = date_part.strip
+          
+          if dates.length < 4
+            puts "error date len < 4 #{dates}, #{s.id}"
+            next
+          end
+          
+          if dates.length >= 4 && dates.length < 12
+            year1 = dates[0, 4]
+            dates033 << year1 if year1.match(/^\d{4}$/)
+          end
+          
+          if dates.length >=12
+            year1 = dates[0, 4]
+            dates033 << year1 if year1.match(/^\d{4}$/)
+            
+            #puts dates.red
+            dates[8, dates.length - 1].split("-").each do |moredate|
+              #puts moredate.green
+              next if moredate == ""
+              next if moredate.length < 4
+              year2 = moredate[0, 4]
+              dates033 << year2 if year2.match(/^\d{4}$/)
+              #puts year2.yellow
+            end
+          end
+          
+        end
+        
+        #puts "#{t.content}\t#{dates033.join("\t")}" if dates033.count > 0
+        #puts "#{t.content}\tERROR" if dates033.count == 0
+        
+        all_dates.concat(dates033)
+        
+      end
+    end
+    
+    if all_dates.count == 0
+      puts "#{s.id}\tNOT PARSABLE"
+      next
+    end
+    
+    # Search in the marc record
+    m = s.marc
+    m.by_tags("033").each {|t| t.destroy_yourself}
+    
+    user = s.user != nil ? s.user.name : "not set"
+    
+    marc_s = m.to_marc
+    puts "MAMME" if marc_s == nil
+    all_dates.each do |d|
+      found =  marc_s.include?(d)
+      puts "#{s.id}\t#{d}\t#{s.lib_siglum}\t#{user}" if !found
+    end
+    
+  end
+end
