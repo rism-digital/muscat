c58c787dc5f281d9b25929be539e94249d2f6617
Rodolfo Zitellini <rwz@xhero.org>
2017-04-12 10:56:17 +0200

    Big dataset printer for 033

--- housekeeping/correct/21_033.rb
+++ housekeeping/correct/21_033.rb
@@ -1,7 +1,19 @@
+def join_values(tag, subtag, marc)
+  values = []
+  marc.by_tags(tag).each do |t|
+    t.each_by_tag(subtag) do |st|
+      next if !st || !st.content
+      values << st.content
+    end
+  end
+  values
+end
+
+puts "id\tcatalog\txml\tdate\tsiglum\tuser\t518$a\t541$d\t260$c\t500$a\t245$a\t246$a"
 Source.find_in_batches do |batch|
   batch.each do |s|
 
-		next if s.source_id != nil
+		#next if s.source_id != nil
 
 		s.marc.load_source false
     # IF none we do not care
@@ -68,16 +80,52 @@ Source.find_in_batches do |batch|
     
     user = s.user != nil ? s.user.name : "not set"
     
-		tag260 = s.marc.root.fetch_all_by_tag("260")
-		
-		tagc = tag260.map {|t| t.fetch_all_by_tag("c").count}
-		tagc = tagc.reduce(0, :+)
+    a = []
+    a << join_values("518", "a", m)
+    a << join_values("541", "d", m)
+    a << join_values("260", "c", m)
+    a << join_values("500", "a", m)
+    a << join_values("245", "a", m)
+    a << join_values("246", "a", m)
 
     marc_s = m.to_marc
     puts "MAMME" if marc_s == nil
+    
     all_dates.each do |d|
       found =  marc_s.include?(d)
-      puts "#{s.id}\t#{d}\t#{s.lib_siglum}\t#{user}\t260: #{tag260.count}\tc: #{tagc}" if !found
+      if !found
+        #ap a
+        link_catalog = "http://admin.rism-ch.org/catalog/#{s.id}"
+        link_admin = "http://admin.rism-ch.org/admin/sources/#{s.id}.xml"
+        print "#{s.id}\t#{link_catalog}\t#{link_admin}\t#{d}\t#{s.lib_siglum}\t#{user}"
+        
+        run = true
+        count = 0
+        while run == true
+
+          print "\t" + (a[0].count-1 >= count ? a[0][count] : "x1")
+          print "\t" + (a[1].count-1 >= count ? a[1][count] : "x2")
+          print "\t" + (a[2].count-1 >= count ? a[2][count] : "x3")
+          print "\t" + (a[3].count-1 >= count ? a[3][count] : "x4")
+          print "\t" + (a[4].count-1 >= count ? a[4][count] : "x5")
+          print "\t" + (a[5].count-1 >= count ? a[5][count] : "x6")
+         
+          #ap a[4][0]
+          #ap count
+          puts
+          count += 1
+         
+          run = false if (count > a[0].count-1 && count > a[1].count-1 &&
+                          count > a[2].count-1 && count > a[3].count-1 &&
+                          count > a[4].count-1 && count > a[5].count-1)
+                         
+          print "\t\t\t\t\t"
+         
+        end
+        
+        puts
+        
+      end
     end
     
   end
