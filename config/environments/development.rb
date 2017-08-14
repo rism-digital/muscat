97ff9951ee85ed1dd31dcd3f24e1e031472c74d2
Rodolfo Zitellini <rwz@xhero.org>
2017-08-14 11:40:07 +0200

    #500 change the event for the link creation, make the DEBUG log rotate!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Muscat::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-	config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+	config.logger = Logger.new("#{Rails.root}/log/#{ENV['RAILS_ENV']}.log", 10, 1048576)
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
