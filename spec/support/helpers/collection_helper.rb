7d6664382317482bb6a928eb476fa53bf2106dd4
stephan <stephan@hirsch-online.com>
2017-08-15 08:39:26 +0200

    Refactor collection flatten method as enumerator

--- spec/support/helpers/collection_helper.rb
+++ spec/support/helpers/collection_helper.rb
@@ -1,7 +1,7 @@
 module CollectionHelper
 
   #This method iterates recursively over all elements of an object and returns them as array
-  def flatten_all(object, result = [])
+  def flatten_all_to_array(object, result = [])
     raise(TypeError, "object is not a collection") unless object.class < Enumerable
     object.each do |e|
       unless e.class < Enumerable
@@ -11,6 +11,21 @@ module CollectionHelper
       end
      end
     return result
+  end
+
+  def flatten_all(object)
+    raise(TypeError, "object is not a collection") unless object.class < Enumerable
+    Enumerator.new do |yielder|
+      object.each do |e|
+        unless e.class < Enumerable
+          yielder << e
+        else
+          flatten_all(e).each do |recursion|
+            yielder << recursion
+          end
+        end
+      end
+    end.lazy
   end
 
 end
