42028be852d53226a35358a581a7938d00121ea5
stephan <stephan@hirsch-online.com>
2018-01-19 12:43:22 +0100

    Changed 931 to 930 && hide the work section for catalogers

--- app/admin/work.rb
+++ app/admin/work.rb
@@ -2,7 +2,7 @@ include Triggers
   
 ActiveAdmin.register Work do
 
-  menu :parent => "indexes_menu", :label => proc {I18n.t(:menu_works)}
+  menu :parent => "indexes_menu", :label => proc {I18n.t(:menu_works)}, :if => proc{ can? :edit, Work  }
 
   # Remove mass-delete action
   batch_action :destroy, false
