c2fd6a215d9a123d883b4a5b67fe6461c803e972
Rodolfo Zitellini <rwz@xhero.org>
2018-01-30 14:54:51 +0100

    Add code to fix bogus catalogues

--- housekeeping/upgrade_4.0/correct_catalogues.rb
+++ housekeeping/upgrade_4.0/correct_catalogues.rb
@@ -11,7 +11,25 @@ we_the_people.each do |pers|
   ap "#{pers.id} #{pers.lock_version}"
   marc = pers.marc
 
-  marc.by_tags("670").each {|t| t.destroy_yourself}
+  marc.by_tags("670").each do |t|
 
+    node = t.deep_copy
+    node.tag = "680"
+    node.indicator = "##"
+    
+    st = node.fetch_first_by_tag("w")
+    st.destroy_yourself if st
+    
+    node.sort_alphabetically
+    marc.root.children.insert(marc.get_insert_position("680"), node)
+    
+    t.destroy_yourself
+  end
+  
   pers.save
+end
+
+Catalogue.where("created_at > '2017-10-11'").where("created_at < '2017-10-12'").each do |c|
+  ap "delete catalogue #{c.id}"
+  c.destroy
 end
