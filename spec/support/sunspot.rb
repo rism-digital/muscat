6f79733b13eb389ffa95daa5a6d35684a97bc3ef
stephan <stephan@hirsch-online.com>
2018-02-07 11:45:01 +0100

    Updated README

--- spec/support/sunspot.rb
+++ spec/support/sunspot.rb
@@ -1,4 +1,17 @@
 RSpec.configure do |config|
-  config.after(:all) do
-  end
-end
+#  config.before(:all) do
+#    sleep 10
+#    puts "Starting solr ...".yellow
+#    %x( RAILS_ENV=test rake sunspot:solr:start )
+#    puts "Solr started at ...".green
+#    sleep 10
+#  end
+#
+#  config.after(:all) do
+#    puts "Stopping solr ....".yellow
+#    %x( RAILS_ENV=test rake sunspot:solr:stop )
+#    puts "Solr stopped in test environment!".green
+#  end
+#
+
+ end
