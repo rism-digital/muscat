c0e2683f4fcbca34566f1d74b1e470b095251455
stephan <stephan@hirsch-online.com>
2018-02-09 09:42:03 +0100

    Fix workgroup example block

--- spec/models/workgroup_spec.rb
+++ spec/models/workgroup_spec.rb
@@ -1,17 +1,26 @@
-RSpec.describe "solr result of workgroup total sources" do
-  let!(:user) { FactoryBot.create(:user)  }
-  it "should return solr result set if source exist" do
-    FactoryBot.create(:manuscript_source)
-    Sunspot.index![Source]
-    ix = Statistics::Workgroup.sources_by_month(Time.parse("2017-01-01"), Time.now, [Workgroup.last])
-    f = Statistics::Spreadsheet.new(ix)
-    expect(f.objects.last.row[Time.now.strftime("%Y-%m")]).to be == 1
+RSpec.describe "Workgroup statistic" do
+  describe "solr result of workgroup total sources" do
+    let!(:user) { FactoryBot.create(:user)  }
+    before do
+      FactoryBot.create(:manuscript_source)
+      Sunspot.index![Source]
+    end
+
+    it "should return solr result set if source exist" do
+      ix = Statistics::Workgroup.sources_by_month(Time.parse("2017-01-01"), Time.now, [Workgroup.last])
+      f = Statistics::Spreadsheet.new(ix)
+      expect(f.objects.last.row[Time.now.strftime("%Y-%m")]).to be == 1
+    end
   end
+ 
+  describe "solr result of workgroup total sources" do
+    let!(:user) { FactoryBot.create(:user)  }
   
-  it "should return zero set if there is no source" do
-    ix = Statistics::Workgroup.sources_by_month(Time.parse("2017-01-01"), Time.now, [Workgroup.last])
-    f = Statistics::Spreadsheet.new(ix)
-    expect(f.objects.last.row[Time.now.strftime("%Y-%m")]).to be == 0
+    it "should return zero set if there is no source" do
+      ix = Statistics::Workgroup.sources_by_month(Time.parse("2017-01-01"), Time.now, [Workgroup.last])
+      f = Statistics::Spreadsheet.new(ix)
+      expect(f.objects.last.row[Time.now.strftime("%Y-%m")]).to be == 0
+    end
   end
 
 end
