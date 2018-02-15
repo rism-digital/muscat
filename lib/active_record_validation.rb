abb2b8099e5d1d98324ad07a64a51808724bb07a
stephan <stephan@hirsch-online.com>
2018-02-15 15:22:57 +0100

    Improved AR validation

--- lib/active_record_validation.rb
+++ lib/active_record_validation.rb
@@ -1,60 +1,58 @@
 module ActiveRecordValidation
 
-  def check_mandatory
+  def checking(level)
     validator = MarcValidator.new(Source.find(self.id), false)
-    validator.rules.each do |k,v|
-      if v["mandatory"]
-        v["mandatory"].each do |subfield, methods|
-          methods.each do |m|
-            self.send(m, k, subfield)
+    validator.rules.each do |datafield, rule|
+      rule.each do |d,options|
+        options.each do |subfield, option|
+          if option.is_a?(Hash)
+            option.each do |k,v|
+              if k == level
+                v.each do |method|
+                  self.send(method, datafield, subfield)
+                end
+              end
+            end
           end
         end
       end
     end
+  end
+
+  def check_mandatory
+    checking "mandatory"
   end
 
   def check_warnings
-    validator = MarcValidator.new(Source.find(self.id), false)
-    validator.rules.each do |k,v|
-      if v["warnings"]
-        v["warnings"].each do |subfield, methods|
-          methods.each do |m|
-            self.send(m, k, subfield)
-          end
-        end
-      end
-    end
+    checking "warnings"
+  end
+
+  def selected_fields(t,s)
+    arry = []
+    marc.each_by_tag(t) { |tag| tag.each_by_tag(s) {|subfield| arry << subfield if subfield.content } }
+    return arry
   end
 
   def must_have_different_id(t,s)
-    marc.each_by_tag(t) do |tag|
-      tag.each_by_tag(s) do |subtag|
-        next unless subtag.content
-        if subtag.content == self.id.to_s
-          errors.add(:base, "#{t}$#{s} must have different id")
-        end
+    selected_fields(t,s).each do |subtag|
+      if subtag.content == self.id.to_s
+        errors.add(:base, "#{t}$#{s} value '#{subtag.content}' must have different id")
       end
     end
   end
 
   def should_be_numeric(t, s)
-    marc.each_by_tag(t) do |tag|
-      tag.each_by_tag(s) do |subtag|
-        next unless subtag.content
-        unless subtag.content =~ /[0-9]/
-          errors.add("#{t}$#{s}", "has non-numeric content")
-        end
+    selected_fields(t,s).each do |subtag|
+      unless subtag.content =~ /[0-9]/
+        errors.add(:base, "#{t}$#{s} value '#{subtag.content}' is not numeric")
       end
     end
   end
 
   def should_be_lt_200(t, s)
-    marc.each_by_tag(t) do |tag|
-      tag.each_by_tag(s) do |subtag|
-        next unless subtag.content
-        unless subtag.content.to_i < 200
-          errors.add("#{t}$#{s}", "is greater than 200")
-        end
+    selected_fields(t,s).each do |subtag|
+      unless subtag.content.to_i < 200
+        errors.add(:base, "#{t}$#{s} value '#{subtag.content}' is greater than 200")
       end
     end
   end
