e91ad2874768519315e370c3ce2b6a7e5765ba34
Rodolfo Zitellini <rwz@xhero.org>
2018-04-03 15:16:12 +0200

    Rescue if the do_* does not have a foreign key (as in when created from scratch with db:migrate)

--- db/migrate/20180403144322_drop_do_tables.rb
+++ db/migrate/20180403144322_drop_do_tables.rb
@@ -1,14 +1,14 @@
 class DropDoTables < ActiveRecord::Migration[4.2]
   def change
-    remove_foreign_key :do_div_files, column: :do_file_id
-    remove_foreign_key :do_div_files, column: :do_div_id
+    remove_foreign_key :do_div_files, column: :do_file_id rescue nil
+    remove_foreign_key :do_div_files, column: :do_div_id rescue nil
     
-    remove_foreign_key :do_divs, column: :do_item_id
+    remove_foreign_key :do_divs, column: :do_item_id rescue nil
     
-    remove_foreign_key :do_file_groups, column: :do_item_id
+    remove_foreign_key :do_file_groups, column: :do_item_id rescue nil
     
-    remove_foreign_key :do_files, column: :do_file_group_id
-    remove_foreign_key :do_files, column: :do_image_id
+    remove_foreign_key :do_files, column: :do_file_group_id rescue nil
+    remove_foreign_key :do_files, column: :do_image_id rescue nil
     
     drop_table :do_items if ActiveRecord::Base.connection.table_exists? 'do_items'
     drop_table :do_div_files if ActiveRecord::Base.connection.table_exists? 'do_div_files'
