7337d281759f2100f199cc95b447914a349fe844
stephan <stephan@hirsch-online.com>
2018-05-09 10:27:34 +0200

    Some new rspecQ

--- spec/views/comments_spec.rb
+++ spec/views/comments_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Sources",  :type => :feature, js: true do
+RSpec.describe "Comments",  :type => :feature, js: true do
   let(:user) { create :admin   }
   before(:each) do
     visit user_session_path
@@ -11,11 +11,11 @@ RSpec.describe "Sources",  :type => :feature, js: true do
 
   describe "Index page" do
     it "should contain the sidebar panel" do
-      visit admin_sources_path
+      visit admin_comments_path
       expect(page).to have_css("#sidebar")
     end
-    it "should current set to sources" do
-      visit admin_sources_path
+    it "should have current set to pagetitle" do
+      visit admin_comments_path
       expect(page.title).to match(Regexp.new(page.find(".current").text))
     end
   end
