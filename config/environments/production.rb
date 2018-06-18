f0264500651c768b5463f44fef862afb4b10ff48
Rodolfo Zitellini <rwz@xhero.org>
2018-06-18 11:10:30 +0200

    Set config.action_mailer.default_url_options[:host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "muscat_#{Rails.env}"
   config.action_mailer.perform_caching = false
+	config.action_mailer.default_url_options = { :host => "muscat.rism.info" }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
