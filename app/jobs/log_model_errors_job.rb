548f57c48d6e2f62b941b35f5b8d23ed184ec04c
Rodolfo Zitellini <rwz@xhero.org>
2018-06-21 14:01:03 +0200

    Add validation in the batch process

--- app/jobs/log_model_errors_job.rb
+++ app/jobs/log_model_errors_job.rb
@@ -14,34 +14,74 @@ class LogModelErrorsJob < ApplicationJob
     new_stdout = StringIO.new
     old_stdout = $stdout
     old_stderr = $stderr
-    $stdout = new_stdout
-    $stderr = new_stdout
-    String.disable_colorization true
+
+    begin_time = Time.now
     
-    count = 0
+    String.disable_colorization true
     
     results = Parallel.map(0..@parallel_jobs, in_processes: @parallel_jobs) do |jobid|
       errors = {}
+      validations = {}
       offset = @limit * jobid
 
       Source.order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
         s = Source.find(sid.id)
         begin
+          ## Capture STDOUT and STDERR
+          ## Only for the marc loading!
+          $stdout = new_stdout
+          $stderr = new_stdout
+          
           s.marc.load_source true
+          
+          # Set back to original
+          $stdout = old_stdout
+          $stderr = old_stderr
+          
+          res = validate_record(s)
+          validations[sid.id] = res if res && !res.empty?
         rescue
+          ## Exit the capture
+          $stdout = old_stdout
+          $stderr = old_stderr
+          
           errors[sid.id] = new_stdout.string
           new_stdout.rewind
         end
         
       end
-      errors
+      {errors: errors, validations: validations}
+    end
+
+    # Extract and separate the errors and validations
+    total_errors = {}
+    total_validations = {}
+    results.each do |r|
+      total_errors.merge!(r[:errors])
+      total_validations.merge!(r[:validations])
     end
     
-    $stdout = old_stdout
-    $stderr = old_stderr
+    end_time = Time.now
     
-    h = results.reduce(&:merge)
-    HealthReport.notify("Source", "Report", h).deliver_now
+    message = "Source report started at #{begin_time.to_s}, (#{end_time - begin_time} seconds run time)"
+    
+    HealthReport.notify("Source", message, total_errors, total_validations).deliver_now
+    
+  end
+  
+  private
+  def validate_record(record)
+    
+    begin
+      validator = MarcValidator.new(record, false)
+      validator.validate
+      validator.validate_dates
+      validator.validate_links
+      validator.validate_unknown_tags
+      return validator.get_errors
+    rescue Exception => e
+      puts e.message
+    end
     
   end
   
--- app/mailers/health_report.rb
+++ app/mailers/health_report.rb
@@ -1,10 +1,11 @@
 class HealthReport < ApplicationMailer
 
-  def notify(model, message, errors)
+  def notify(model, message, errors, validations)
     
     @message = message
 		@model = model
 		@errors = errors
+		@validations = validations
     
     mail(to: RISM::NOTIFICATION_EMAIL,
         name: "Muscat Admin Notificator",
