e425a4e41372e08a2ff0a7a5735a3ec3f9850510
stephan <stephan@hirsch-online.com>
2018-08-13 16:22:10 +0200

    Added 535 to marc conf for editions closes #493

--- spec/controllers/edition_original_spec.rb
+++ spec/controllers/edition_original_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Admin::SourcesController, :type => :controller do
   describe "Edition" do
     it "there should be the possibibility to specify the master holding in 535" do
       visit edit_admin_source_path(edition)
-      expect(page).to have_css("[data-tag='535'] [data-subfield='a']")
+      expect(page).to have_css("[data-tag='535']")
       #element = page.find("[data-tag='535'] [data-subfield='a'")"]")
     end
   end
