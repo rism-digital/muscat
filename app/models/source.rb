36ef37bab41cc517ebe793a9a46b8ffd50149eff
stephan <stephan@hirsch-online.com>
2018-10-29 09:14:19 +0100

    Added holdings.wf_owner to sources index

--- app/models/source.rb
+++ app/models/source.rb
@@ -250,7 +250,10 @@ class Source < ApplicationRecord
 #      date_to != nil && date_to > 0 ? date_to : nil
 #    end
     
-    sunspot_dsl.integer :wf_owner
+    sunspot_dsl.integer :wf_owner, multiple: true do |s|
+      s.holdings.map {|e| e.wf_owner} << s.wf_owner
+    end
+
     sunspot_dsl.string :wf_stage
     sunspot_dsl.time :updated_at
     sunspot_dsl.time :created_at, multiple: true do |s|
