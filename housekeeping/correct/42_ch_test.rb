664c7bdb61a3c881908c65b5dee144342083ae46
Rodolfo Zitellini <rwz@xhero.org>
2018-12-14 09:10:13 +0100

    Make import pass on duplicates

---
+++ housekeeping/correct/42_ch_test.rb
@@ -0,0 +1,14 @@
+Source.where("id > 400000000 and id < 420000000").each do |se|
+	s = Source.find(se.id) 
+	puts s.id
+	s.marc.load_source(false)
+	s.marc.import
+	
+	s.suppress_reindex
+	s.suppress_update_77x
+	s.suppress_update_count
+	
+	s.save
+	s = nil
+end
+	
