972007cd2676156be674cc30fdd8ea529733a960
stephan <stephan@hirsch-online.com>
2019-01-21 12:15:45 +0100

    Adding some basic validation with merge

--- lib/merge_controller_actions.rb
+++ lib/merge_controller_actions.rb
@@ -7,9 +7,18 @@ require 'resource_dsl_extensions.rb'
 module MergeControllerActions
   
   def self.included(dsl)
-    dsl.batch_action :merge, if: proc{ current_user.has_role?('admin')   }  do |ids|
-      model = controller_path.classify.gsub(/^.+\:\:/, "")
-      redirect_to request.referer, notice: "Auth merged! #{ids} #{model}"
+    dsl.batch_action :merge, confirm: "Are you sure?", if: proc{ current_user.has_role?('admin') || current_user.has_role?('editor') }  do |ids|
+      model = self.resource_class
+      if ids.size != 2
+        redirect_to request.referer, alert: "Too many entries selected!"      
+      else  
+        collection = model.find(ids)
+        if collection.pluck(:wf_stage).sort == ["inprogress", "published"]
+          redirect_to request.referer, notice: "Auth merged! #{ids} #{model}"
+        else
+          redirect_to request.referer, alert: "Not containing published and unpublished items!"      
+        end
+      end
     end
   end
   
