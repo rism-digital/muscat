64c769e00b611c0ece9c3ee7fcfb89746970db49
bjoernkessler <bjoern.kessler@rism.info>
2019-02-05 13:41:32 +0100

    marc_config is done

--- lib/marc_config.rb
+++ lib/marc_config.rb
@@ -1,20 +1,23 @@
 require 'yaml'
 
-# Load the Marc Configuration. It is used to define how various tags should be handles
-# shown or edited. It also specifies how indicators and subtags are configured.
+# Loads the Marc Configuration. 
+# Defines how various Tags are handled, shown or edited.
+# Also specifies Configuration of Indicators and Subtags.
 # The tag config is hardcoded in /config/marc/tag_config.5.0.yml
-#
+
 class MarcConfig
 
-  # create a new Instance of a Marc Configuration
-  # @param tag_config_file_path (see #load_config)
+  # Creates a new Instance of a Marc Configuration
+  # @see #load_config
+  # @param tag_config_file_path [String] Path to Configuration (YAML) File
+  # @return [MarcConfig]
   def initialize(tag_config_file_path)
     @model = load_config tag_config_file_path
   end
 
   private
 
-  # load Marc Configuration from File
+  # Loads Marc Configuration from File
   # @param tag_config_file_path [String] Path to Configuration (YAML) File
   # @param overlay_path [String] Path to Overlay File
   # @return [MarcConfig]
@@ -66,7 +69,7 @@ class MarcConfig
   public
 
   # Adds overlay to the Model
-  # (see #load_config)
+  # @param (see #load_config)
   def add_overlay(tag_config_file_path, overlay_path)
     @model = load_config tag_config_file_path, overlay_path
   end
@@ -115,8 +118,8 @@ class MarcConfig
   end
 
   # Checks if a Tag and a Subtag should link to a foreign Class (i.e. People)
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
   # @return [Integer] Index of the Tag plus the Subtag or false
   # @todo Would be nicer to return 0 if no Tag and Subtag are found
   def is_foreign?(tag, subtag)
@@ -128,8 +131,8 @@ class MarcConfig
   end
 
   # Gets the disable_create_lookup Flag
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
   # @return [Boolean]
   # @todo The return flag should be enough actually
   def disable_create_lookup?(tag, subtag)
@@ -139,48 +142,48 @@ class MarcConfig
   end
 
   # Gets the foreign class for a tag and subtag
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
-  # @return [String]
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [String] Foreign Class Name
   def get_foreign_class(tag, subtag)
     @tag_config[tag][:fields].assoc(subtag)[1][:foreign_class]
   end
 
   # Gets the foreign field that is connected to a foreign class
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
-  # @return [String]
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [String] Foreign Field Name
   def get_foreign_field(tag, subtag)
     @tag_config[tag][:fields].assoc(subtag)[1][:foreign_field]
   end
 
   # Gets the foreign alternative Field Names
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
-  # @return [String]
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [String] Foreign Alternatives
   def get_foreign_alternates(tag, subtag)
     @tag_config[tag][:fields].assoc(subtag)[1][:foreign_alternates]
   end
 
   # Gets the foreign Dependents
-  # @param [String] Tag Name
-  # @param [String] Subtag Name
-  # @return [String] ???
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [String] Foreign Dependents
   # @todo Is this still used? What ist the return value?
   def get_foreign_dependants(tag, subtag)
     return @foreign_dependants[tag + subtag]
   end
 
   # Looks for the Number of foreign Subfields
-  # @param [String] Tag Name
+  # @param tag [String] Tag Name
   # @return [Integer] Number of foreign Subfields or nil
-  # @todo when and why is there going to be nil
+  # @todo when and why is there going to be nil, when tag is empty?
   def has_foreign_subfields(tag)
     return @foreign_tag_groups.rindex(tag)
   end
 
   # Gets tags with references to a given Model
-  # @param [String] Model Name
+  # @param link_model [String] Model Name
   # @return [Array<String>] Tag Names
   def get_remote_tags_for(link_model)
     remote_tags = []
@@ -234,7 +237,11 @@ class MarcConfig
 	  end
   end
 
-  # Get the foreign field 0 padding length for string field (if wanted)
+  # Gets the foreign field 0 padding length for string field (if wanted)
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Nil]
+  # @todo looks strange. Is it really a getter-Function? rescue nil is bad! Use try?
   def get_zero_padding(tag, subtag = "")
     # p tag
     # p subtag
@@ -245,11 +252,14 @@ class MarcConfig
     end
   end
 
-  # Check if a tag or subtag can be repeated (* or + mean it is)
+  # Checks if a Tag or Subtag can be repeated (* or + mean it is)
   # disable_multiple means that duplication is disable IN EDITOR
   # but the field is allowed to be repeatable IN MARC
-  # This is for tag groups: a field should not be repeatable in the group
-  # but can be repeated because of multiple groups
+  # This is for Tag groups: a Field should not be repeatable in the Group
+  # but can be repeated because of multiple Groups
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
   def multiples_allowed?(tag, subtag = "")
     disable_multiple = false
     if subtag.empty?
@@ -264,54 +274,98 @@ class MarcConfig
     return false
   end
 
+  # Gets a Tags Master-Subtag
+  # @param tag [String] Tag Name
+  # @return [String] Master-Subtag
   def get_master(tag)
     @tag_config[tag][:master]
   end
 
+
+  # Gets the master_optional flag
+  # @param tag [String] Tag Name
+  # @todo Is this still used? Could not find any applications in the Models!
   def master_optional?(tag)
     @tag_config[tag][:master_optional]
   end
 
+  # Tests for the Existence of Tag in the Marc Configuration
+  # @param tag [String] Tag Name
+  # @return [Boolean]
   def has_tag?(tag)
     return @tag_config.include?(tag)
   end
 
+  # Tests for the Existence of Subtag in the Marc Configuration
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
   def has_subfield?(tag, subtag )
     return true if @tag_config[tag][:fields].assoc(subtag)
     return false
   end
 
+  # Tests Tags whether they are having Subtags
+  # @param tag [String] Tag Name
+  # @return [Boolean]
   def is_tagless?(tag)
     @tag_config[tag][:tagless]
   end
 
-  # Return if a tag is not browsable
+  # Tests whether a Tag is not browsable
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
+  # @todo replace rescue nil with try
   def show_in_browse?(tag, subtag)
     s = subtag.gsub(/\$/,"")
     !@tag_config[tag][:fields].assoc(s)[1][:no_browse] rescue nil
   end
 
-  # Return if a tag should be hidden
+  # Tests whether a Tag should be hidden
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
+  # @todo replace rescue nil with try
   def always_hide?(tag, subtag)
     s = subtag.gsub(/\$/,"")
     @tag_config[tag][:fields].assoc(s)[1][:no_show] rescue nil
   end
 
+  # Tests whether Tag is inline
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
+  # @todo replace rescue nil with try
   def browse_inline?(tag, subtag)
     s = subtag.gsub(/\$/,"")
     @tag_config[tag][:fields].assoc(s)[1][:browse_inline] rescue nil
   end
 
+  # Tests for Browse Helber
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Boolean]
   def get_browse_helper(tag, subtag)
     s = subtag.gsub(/\$/,"")
     @tag_config[tag][:fields].assoc(s)[1][:browse_helper]
   end
 
+
+  # Gets the Size of the Tag
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [Integer] Tag Size
   def get_size(tag, subtag)
     s = subtag.gsub(/\$/,"")
     @tag_config[tag][:fields].assoc(s)[1][:size] || 15
   end
 
+  # Gets the Values of the Attributes of a Subtag
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @param attribute_name [Symbol] Attribute
+  # @return [String] Attribute Value
   def get_subtag_attribute(tag, subtag, attribute_name)
     pull = @tag_config[tag]
     if pull
@@ -323,8 +377,12 @@ class MarcConfig
     return nil
   end
 
+  # Gets the Indicators
+  # @param tag [String] Tag Name
+  # @param subtag [String] Subtag Name
+  # @return [String] Indicator
+  #@todo: THIS IS ALL WRONG - indicator is now in top level - FIX!
   def get_indicator(tag, subtag = "")
-  #TODO: THIS IS ALL WRONG - indicator is now in top level - FIX!
     if @tag_config[tag][:fields].assoc(subtag)[1][:indicator].is_a? Array
       return @tag_config[tag][:fields].assoc(subtag)[1][:indicator][0]
     else
@@ -332,14 +390,19 @@ class MarcConfig
     end
   end
 
+  # Iterates over every Data-Tag
   def each_data_tag
     @tag_config.keys.sort.each { |tag| yield tag if tag.to_i > 8 }
   end
 
+  # Iterates over every Subtag of a Tag
   def each_subtag( tag )
     @tag_config[tag][:fields].each { |subtag| yield subtag }
   end
 
+  # Gets every Tag containing Subtag
+  # @param subtag [String] Subtag Name
+  # @return [Array<String>] Tags
   def tags_with_subtag( subtag )
     tags = Array.new
     @tag_config.keys.sort.each do |tag|
@@ -348,6 +411,8 @@ class MarcConfig
     tags
   end
 
+  # Some magic functionality
+  # @todo seems to be deprecated, no usecase found
   def dive(struct, from)
     struct.each do |k, v|
       if v.is_a?(Hash) and from[k]
@@ -358,6 +423,8 @@ class MarcConfig
     end
   end
 
+  # Some magic functionality
+  # @todo seems to be deprecated, no usecase found
   def squeeze(other)
     other.each do |k, v|
       if v.is_a?(Hash) and @whole_config[k]
