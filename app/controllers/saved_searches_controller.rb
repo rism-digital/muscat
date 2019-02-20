0e88d9bc78f3a492d2583f07876069790aca8d33
Rodolfo Zitellini <rwz@xhero.org>
2019-02-20 12:00:52 +0100

    Migrate from glyphicons, fix advanced search

--- app/controllers/saved_searches_controller.rb
+++ app/controllers/saved_searches_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
+## FIXME!!
 class SavedSearchesController < ApplicationController
-  include Blacklight::SavedSearches
+#  include Blacklight::SavedSearches
 
-  helper BlacklightAdvancedSearch::RenderConstraintsOverride
+#  helper BlacklightAdvancedSearch::RenderConstraintsOverride
 end
