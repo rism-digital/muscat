6204e43bbb85f43f66a41cacacc3914941c9a23e
Rodolfo Zitellini <rwz@xhero.org>
2019-09-16 13:59:42 +0200

    DISABLE CHECKS TO MAKE AUTHORITY ID CHANGE POSSIBLE ONLY FOR CH MIGRATION liturgical_feast, standard_term and standard_title tables need to be modified to accept NULL values on term, name and title

--- app/models/liturgical_feast.rb
+++ app/models/liturgical_feast.rb
@@ -17,9 +17,9 @@ class LiturgicalFeast < ApplicationRecord
   has_many :delayed_jobs, -> { where parent_type: "LiturgicalFeast" }, class_name: 'Delayed::Backend::ActiveRecord::Job', foreign_key: "parent_id"
   belongs_to :user, :foreign_key => "wf_owner"
     
-  validates_presence_of :name
+  #validates_presence_of :name
   
-  validates_uniqueness_of :name
+  #validates_uniqueness_of :name
   
   #include NewIds
   
