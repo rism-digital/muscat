af5d2c2a0e2c8f067d8695b9ab82dd97aadb2723
Rodolfo Zitellini <rwz@xhero.org>
2019-09-24 13:12:02 +0200

    Make it parallel

--- housekeeping/upgrade_ch/02_migrate_authority_ids.rb
+++ housekeeping/upgrade_ch/02_migrate_authority_ids.rb
@@ -31,47 +31,47 @@ def remove_marc_tag(marc, tag)
 end
 
 print "Loading new ids "
-new_person_ids = {}
-old_person_ids = {}
+@new_person_ids = {}
+@old_person_ids = {}
 CSV.foreach("housekeeping/upgrade_ch/people_newids.csv") do |r|
     # format is CH id, BM id
-    new_person_ids[r[1].to_i] = r[0].to_i
-    old_person_ids[r[0].to_i] = r[1].to_i
+    @new_person_ids[r[1].to_i] = r[0].to_i
+    @old_person_ids[r[0].to_i] = r[1].to_i
 end
 print "."
 
-old_650_ids = {}
+@old_650_ids = {}
 # Format is BM id, CH id
 CSV.foreach("housekeeping/upgrade_ch/changed_650.tsv", col_sep: "\t") do |r|
-    old_650_ids[r[2].to_i] = r[0].to_i
+    @old_650_ids[r[2].to_i] = r[0].to_i
 end
 print "."
 
-old_657_ids = {}
+@old_657_ids = {}
 # Format is BM id, CH id
 CSV.foreach("housekeeping/upgrade_ch/changed_657.tsv", col_sep: "\t") do |r|
-    old_657_ids[r[2].to_i] = r[0].to_i
+    @old_657_ids[r[2].to_i] = r[0].to_i
 end
 print "."
 
-old_690_ids = {}
+@old_690_ids = {}
 # Format is BM id, CH id
 CSV.foreach("housekeeping/upgrade_ch/changed_690.tsv", col_sep: "\t") do |r|
-    old_690_ids[r[2].to_i] = r[0].to_i
+    @old_690_ids[r[2].to_i] = r[0].to_i
 end
 print "."
 
-old_240_ids = {}
+@old_240_ids = {}
 # Format is BM id, CH id
 CSV.foreach("housekeeping/upgrade_ch/changed_240.tsv", col_sep: "\t") do |r|
-    old_240_ids[r[2].to_i] = r[0].to_i
+    @old_240_ids[r[2].to_i] = r[0].to_i
 end
 print "."
 
-old_651_ids = {}
+@old_651_ids = {}
 # Format is BM id, CH id
 CSV.foreach("housekeeping/upgrade_ch/changed_651.tsv", col_sep: "\t") do |r|
-    old_651_ids[r[2].to_i] = r[0].to_i
+    @old_651_ids[r[2].to_i] = r[0].to_i
 end
 print "."
 
@@ -79,112 +79,102 @@ print "."
 # 30000010	CH-MÜ	50000010	CH-MÜ
 # 50006835 => 30079405 is CH-FI, which did not show up?
 # 50007932 => 30080410 CH-NYan
-old_852_ids = {50000010 => 30000010, 50006835 => 30079405, 50007932 => 30080410}
+@old_852_ids = {50000010 => 30000010, 50006835 => 30079405, 50007932 => 30080410}
 print "."
 puts " done."
 
-URL = "http://dev.muscat-project.org/catalog/"
-
-check_added = false
-
-pb = ProgressBar.new(Source.count)
-
-Source.all.each do |orig_source|
+def migrate_source(orig_source)
     mod = false
-    pb.increment!
-    #m = Net::HTTP.get(URI(URL + "#{orig_source.id}.txt"))
-
-    #bmmarc = MarcSource.new(m)
-    #bmmarc.load_source(false)
+    #pb.increment!
 
     chmarc = orig_source.marc
 
     chmarc.each_by_tag("100") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_person_ids.include?(id)
-            #puts "100 replace #{id} with #{old_person_ids[id]}".green
+        if @old_person_ids.include?(id)
+            #puts "100 replace #{id} with #{@old_person_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_person_ids[id])
+            replace_single_subtag(t, "0", @old_person_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("700") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_person_ids.include?(id)
-            #puts "700 replace #{id} with #{old_person_ids[id]}".green
+        if @old_person_ids.include?(id)
+            #puts "700 replace #{id} with #{@old_person_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_person_ids[id])
+            replace_single_subtag(t, "0", @old_person_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("650") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_650_ids.include?(id)
-            #puts "650 replace #{id} with #{old_650_ids[id]}".green
+        if @old_650_ids.include?(id)
+            #puts "650 replace #{id} with #{@old_650_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_650_ids[id])
+            replace_single_subtag(t, "0", @old_650_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("657") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_657_ids.include?(id)
-            #puts "657 replace #{id} with #{old_657_ids[id]}".green
+        if @old_657_ids.include?(id)
+            #puts "657 replace #{id} with #{@old_657_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_657_ids[id])
+            replace_single_subtag(t, "0", @old_657_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("690") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_690_ids.include?(id)
-            #puts "690 replace #{id} with #{old_690_ids[id]}".green
+        if @old_690_ids.include?(id)
+            #puts "690 replace #{id} with #{@old_690_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_690_ids[id])
+            replace_single_subtag(t, "0", @old_690_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("651") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_651_ids.include?(id)
-            puts "651 replace #{id} with #{old_651_ids[id]}".green
+        if @old_651_ids.include?(id)
+            #puts "651 replace #{id} with #{@old_651_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_651_ids[id])
+            replace_single_subtag(t, "0", @old_651_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("691") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_690_ids.include?(id)
-            #puts "690 replace #{id} with #{old_690_ids[id]}".green
+        if @old_690_ids.include?(id)
+            #puts "690 replace #{id} with #{@old_690_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_690_ids[id])
+            replace_single_subtag(t, "0", @old_690_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("240") do |t|
         id = fetch_single_subtag(t, "0")
-        if old_240_ids.include?(id)
-            #puts "240 replace #{id} with #{old_240_ids[id]}".green
+        if @old_240_ids.include?(id)
+            #puts "240 replace #{id} with #{@old_240_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_240_ids[id])
+            replace_single_subtag(t, "0", @old_240_ids[id])
             mod = true
         end
     end
 
     chmarc.each_by_tag("852") do |t|
         id = fetch_single_subtag(t, "x")
-        if old_852_ids.include?(id)
-            #puts "240 replace #{id} with #{old_240_ids[id]}".green
+        if @old_852_ids.include?(id)
+            puts "852 replace #{id} with #{@old_240_ids[id]}".green
             delete_single_subtag(t, "a")
-            replace_single_subtag(t, "0", old_852_ids[id])
+            replace_single_subtag(t, "0", @old_852_ids[id])
             mod = true
         end
     end
@@ -213,3 +203,26 @@ Source.all.each do |orig_source|
     orig_source.save if mod
 end
 
+
+#pb = ProgressBar.new(Source.count)
+
+# Non parallel version
+#Source.all.each do |orig_source|
+#    migrate_source(orig_source)
+#end
+
+@parallel_jobs = 10
+@all_src = Source.all.count
+@limit = @all_src / @parallel_jobs
+
+begin_time = Time.now
+  
+results = Parallel.map(0..@parallel_jobs, in_processes: @parallel_jobs, progress: "Saving sources") do |jobid|
+  offset = @limit * jobid
+
+  Source.order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
+    s = Source.find(sid.id)
+    migrate_source(s)
+    s = nil
+  end
+end
