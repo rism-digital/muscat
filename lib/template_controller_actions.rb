d07a41aead96923f1ceabebe0820b30676246fe9
stephan <stephan@hirsch-online.com>
2019-10-01 10:06:10 +0200

    Build batch action

---
+++ lib/template_controller_actions.rb
@@ -0,0 +1,23 @@
+# Override collection_action so it is public
+# from activeadmin lib/active_admin/resource_dsl.rb
+require 'resource_dsl_extensions.rb'
+
+module TemplateControllerActions
+  
+  def self.included(dsl)
+    dsl.batch_action :change_template, form: {
+      target_template: MarcSource::RECORD_TYPES.to_a.select{|k,v| k if v!=0}.map{|k,v| ["#{I18n.t('record_types.' + k.to_s)}",v]}.sort,
+    } do |ids, inputs|
+        sources = Source.where(id: ids)
+        target_template = inputs[:target_template].to_i
+        sources.each do |source|
+          source.change_template_to(target_template)
+        end
+
+        redirect_to collection_path#, :notice => I18n.t(:success, scope: :folders, name: inputs[:name], count: results.count)
+    end
+     
+  end
+  
+  
+end
