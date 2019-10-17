1522972efea50191f9852b79e7b0143a06d3de4c
Rodolfo Zitellini <rwz@xhero.org>
2019-10-17 15:46:10 +0200

    Latest revision

--- housekeeping/correct/42_ch_test.rb
+++ housekeeping/correct/42_ch_test.rb
@@ -1,6 +1,9 @@
+pb = ProgressBar.new(Source.where("id > 400000000 and id < 420000000").count)
+
 Source.where("id > 400000000 and id < 420000000").each do |se|
 	s = Source.find(se.id) 
-	puts s.id
+	#puts s.id
+	pb.increment!
 	s.marc.load_source(false)
 	s.marc.import
 	
