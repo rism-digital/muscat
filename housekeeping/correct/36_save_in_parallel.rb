1d3b855b9d6eb09ae7c4ba0e866113abae3c767f
Rodolfo Zitellini <rwz@xhero.org>
2019-10-21 22:00:09 +0200

    Revert "Special version of save in parallel"
    This reverts commit 5d62a3c560365db1af715684c298a7bb6a8fd126.

--- housekeeping/correct/36_save_in_parallel.rb
+++ housekeeping/correct/36_save_in_parallel.rb
@@ -1,5 +1,5 @@
 @parallel_jobs = 10
-@all_src = Source.where("id > 400000000 and id < 420000000").count
+@all_src = Source.all.count
 @limit = @all_src / @parallel_jobs
 
 begin_time = Time.now
@@ -7,19 +7,19 @@ begin_time = Time.now
 results = Parallel.map(0..@parallel_jobs, in_processes: @parallel_jobs, progress: "Saving sources") do |jobid|
   offset = @limit * jobid
 
-  Source.where("id > 400000000 and id < 420000000").order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
-	puts sid.id
+  Source.order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
     s = Source.find(sid.id)
     
-	s.marc.load_source(false)
-	s.marc.import
-	
-	s.suppress_reindex
-	s.suppress_update_77x
-	s.suppress_update_count
-
-	s.save
-    s = nil
+  	s.suppress_update_77x
+  	s.suppress_update_count
+  	s.suppress_reindex
+  	begin
+  		s.save!
+  	rescue => e
+  		puts "Could not save source #{s.id}"
+  		puts e.exception
+  	end
+    
   end
 end
 
