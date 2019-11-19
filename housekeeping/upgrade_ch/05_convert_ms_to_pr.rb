0becfd442af2607325226f5dd92fe7b6216bbd3b
Rodolfo Zitellini <rwz@xhero.org>
2019-11-19 14:52:53 +0100

    Fix the 500s, hopefully definitively...

--- housekeeping/upgrade_ch/05_convert_ms_to_pr.rb
+++ housekeeping/upgrade_ch/05_convert_ms_to_pr.rb
@@ -97,6 +97,56 @@ def tag_to_text(marc, tag, groups)
 
 end
 
+def groups_to_human_readable_text(marc, group)
+    tokens = []
+
+    tags = marc.by_tags_with_subtag(["593"], "8", group)
+    tags.each do |t|
+        tokens << fetch_single_subtag(t, "a")
+    end
+
+    tags = marc.by_tags_with_subtag(["260"], "8", group)
+    tags.each do |t|
+        info = []
+        t.each_by_tag("a") do |st|
+            next if !st || !st.content
+            info << st.content
+        end
+        info << fetch_single_subtag(t, "b")
+        info << fetch_single_subtag(t, "c")
+        info << fetch_single_subtag(t, "e")
+        info << fetch_single_subtag(t, "r")
+        tokens << info.compact.join(", ")
+    end
+
+    tags = marc.by_tags_with_subtag(["300"], "8", group)
+    tags.each do |t|
+        info = []
+        t.each_by_tag("a") do |st|
+            next if !st || !st.content
+            info << st.content
+        end
+
+        t.each_by_tag("b") do |st|
+            next if !st || !st.content
+            info << st.content
+        end
+
+        info << fetch_single_subtag(t, "c")
+        tokens << info.compact.join(", ")
+    end
+
+    tags = marc.by_tags_with_subtag(["590"], "8", group)
+    tags.each do |t|
+        info = []
+        info << fetch_single_subtag(t, "a")
+        info << fetch_single_subtag(t, "b")
+        tokens << info.compact.join(", ")
+    end
+
+    tokens.compact.join("; ")
+end
+
 def copy_group(marc, new_marc, group)
     marc.all_tags.each do |tgs|
         grp = tgs.fetch_first_by_tag("8")
@@ -290,7 +340,8 @@ def create_holding(row, source, marc, replace = nil, old_siglum = nil, only_grou
     end
 
     # Insert the 500 note, only if Z is filled
-    insert_single_marc_tag(new_marc, "500", "a", row[:ac]) if row[:z]
+    # or if this is a result of a split
+    insert_single_marc_tag(new_marc, "500", "a", row[:ac]) if row[:z] || row[:w].include?("split")
     
     # Save the holding
     new_marc.suppress_scaffold_links
@@ -355,13 +406,17 @@ def migrate_children(source, new_id = false, purge_groups = false)
             note = ["593", "260", "300", "590"].map do |tag|
                 tag_to_text(child.marc, tag, purge_groups)
             end.compact.join("\n")
-        
+
             purge_groups.each do |grp|
+                # Preserve the old groups as human readable note
+                human_note = groups_to_human_readable_text(child.marc, format('%02d', grp).to_str)
+                insert_single_marc_tag(child.marc, "500", "a", "Additional material group: " + human_note)
+
                 delete_group(child.marc, grp)
             end
         
-            # Add the note in the 500
-            insert_single_marc_tag(child.marc, "500", "a", "Additional material group(s): " + note)
+            # ...and non human readable one
+            insert_single_marc_tag(child.marc, "599", "a", "Deleted groups: " + note)
         end
 
         # Migrate the tags only if it was not already migrated
@@ -493,8 +548,8 @@ def merge(row, s, do_merge = true)
 
     puts "Saving #{a1_rec.id}".blue
 
-    # Insert the 500 note in the BM record, for delete and merge
-    insert_single_marc_tag(a1_rec.marc, "500", "a", row[:ac]) 
+    # Insert the 500 note in the BM record, for merge
+    insert_single_marc_tag(a1_rec.marc, "500", "a", row[:ac]) if do_merge
 
     a1_rec.suppress_reindex
     a1_rec.suppress_update_count
@@ -533,11 +588,14 @@ def purge(row, s)
     end.compact.join("\n")
 
     groups.each do |grp|
+        human_note =  groups_to_human_readable_text(s.marc, format('%02d', grp).to_str)
+        insert_single_marc_tag(s.marc, "500", "a", "Additional material group: " + human_note)
+
         delete_group(s.marc, grp)
     end
 
-    # Add the note in the 500
-    insert_single_marc_tag(s.marc, "500", "a", "Additional material group(s): " + note)
+    # As above non human readable one in 599
+    insert_single_marc_tag(s.marc, "599", "a", "Deleted groups: " + note)
 
     s.suppress_reindex
     s.suppress_update_count
@@ -588,7 +646,7 @@ def split(row, s)
     create_holding(row, a1_rec, s.marc, replace, s.lib_siglum, group)
 
     # Save the a1 record
-    insert_single_marc_tag(a1_rec.marc, "500", "a", row[:ac]) 
+    insert_single_marc_tag(a1_rec.marc, "500", "a", row[:ac])
 
     a1_rec.suppress_reindex
     a1_rec.suppress_update_count
@@ -627,7 +685,7 @@ CSV::foreach("housekeeping/upgrade_ch/migrate_ms.csv", quote_char: '~', col_sep:
 
     next if r[:w].include? "man."
 
-    #next if r[:d] != "402009390"
+    #next if r[:d] != "402007083"
 
     begin
         s = Source.find(r[:d])
