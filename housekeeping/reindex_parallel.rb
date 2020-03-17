9bfc277451c7c5d91760a7aefa8882c18190dbf3
Rodolfo Zitellini <rwz@xhero.org>
2020-03-17 15:33:27 +0100

    New reindex script

--- housekeeping/reindex_parallel.rb
+++ housekeeping/reindex_parallel.rb
@@ -1,59 +1,33 @@
-require "progress_bar"
-
 @parallel_jobs = 10
 @all_src = Source.all.count
 @limit = @all_src / @parallel_jobs
 
-jobnr = ARGV[0]
-jobnr = 0 if !jobnr
-jobnr = jobnr.to_i if jobnr
-
-pb = ProgressBar.new(@limit)
-
-#for jobnr in 0..@parallel_jobs
-  offset = @limit * jobnr
-  #Get the first record ID and last record id for this batch
-
-  begin
-    first_id = Source.order(:id).limit(@limit).offset(offset).select(:id).first.id  
-    last_id = Source.order(:id).limit(@limit).offset(offset).select(:id).last.id
-    puts "First #{jobnr} #{first_id}, last #{last_id}"
-  rescue NoMethodError
-    # This is the last one
-    if jobnr == @parallel_jobs
-      puts "Arrived to last job, offset was #{offset}, total src #{@all_src}"
-    else
-      # THis is an actual error!
-      return -1
-    end
-  end
+begin_time = Time.now
   
-  count = 0
-  Source.order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
-    record = Source.find(sid.id)
-    begin
-      Sunspot.index record
-    rescue => e
-      puts "Could not load #{sid.id}: #{e.exception}"
+results = Parallel.map(0..@parallel_jobs - 1, in_processes: @parallel_jobs, progress: "Reindexing sources") do |jobid|
+    offset = @limit * jobid
+    count = 0
+    e_count = 0
+    Source.order(:id).limit(@limit).offset(offset).select(:id).each do |sid|
+        s = Source.find(sid.id)
+    
+        begin
+            Sunspot.index s
+            count += 1
+        rescue => e
+            puts "Could not load #{sid.id}: #{e.exception}"
+            e_count += 1
+        end
     end
-    #record.reindex
-    #Sunspot.commit
-    pb.increment!
-    count += 1
-    if count == 50
-      # In SOLR 5 we use the autocommit
-      #Sunspot.commit
-      count = 0
-    end
-    record = nil
-  end
-  
-#  batch = 1
-#  Source.find_in_batches(start: first_id, batch_size: 50) do |group|
-#    Sunspot.index group
-#    Sunspot.commit
-#    pb.increment!(50)
-#    batch += 1
-#  end
-  
-#end
+    [count, e_count]
+end
+
+end_time = Time.now
+puts "Reindex saving started at #{begin_time.to_s}, ended at: #{end_time.to_s}"
+puts "(#{end_time - begin_time} seconds run time)"
+puts "Results are: #{results.to_s}"
+
+indexed_sources = results.inject(0){|n, item| n += item[0]}
+error_sources = results.inject(0){|n, item| n += item[1]}
+
+puts "Indexed sources: #{indexed_sources}, Unloadable sources: #{error_sources}"
