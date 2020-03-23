cf161665084fc9fecdf4e9c3cc0ee1ead3f4f860
Rodolfo Zitellini <rwz@xhero.org>
2020-03-23 13:10:32 +0100

    Fix #912, make triggers work with 1 source

--- app/mailers/modification_notification.rb
+++ app/mailers/modification_notification.rb
@@ -8,7 +8,15 @@ class ModificationNotification < ApplicationMailer
     
     return if !@user || !@user.email
 
-    subject = @results.count > 1 ? "Source modification report: #{@results.count} records" : "Source #{@results.first[0]} was modified"
+    if @results.count > 1
+      subject = "Source modification report: #{@results.count} records"
+    else
+      s = results_by_criteria.values.first[0]
+
+      composer = !s.composer.empty? ? s.composer : "n.a."
+      title = !s.std_title.empty? ? s.std_title : "none"
+      subject = "Source #{composer}: #{title} (#{s.id}) was modified"
+    end
 
     mail(to: @user.email,
         from: "#{RISM::DEFAULT_EMAIL_NAME} Modification Notificator <#{RISM::DEFAULT_NOREPLY_EMAIL}>",
