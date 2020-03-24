5f3582e53dd705fffba21791d8d0e8cd8bcf432b
Rodolfo Zitellini <rwz@xhero.org>
2020-03-24 12:55:09 +0100

    Keep the posision

--- lib/blacklight_remove_facet.rb
+++ lib/blacklight_remove_facet.rb
@@ -10,19 +10,35 @@ module Blacklight
   
         
         def add_blacklight_field config_key, *args, &block
-            
-            if args.count > 1
-                if args[1].is_a? Hash
-                    if args[1].include?(:override) && args[1][:override] == true
-                        self[config_key.pluralize].delete(args[0])
-                    end
+            override = false
+            insert_position = 0
+            subhash = nil
+
+            if args.count > 1 && args[1].is_a?(Hash)
+                if args[1].include?(:override) && args[1][:override] == true
+                    override = true
+                    # Get the current position to insert it later
+                    subhash = self[config_key.pluralize].to_a
+                    insert_position = subhash.index(subhash.assoc(args[0]))
+                    # Remove from hash
+                    self[config_key.pluralize].delete(args[0])
                 end
             end
 
+            # Call the original function, unless :override is set
+            # it is just pass-through
             old_add_blacklight_field config_key, *args, &block
-            return;
-            
-          self[config_key.pluralize][ field_config.key ] = field_config
+
+            # Move the facet to the current posision
+            if override
+                # Save the newly created element
+                element = self[config_key.pluralize][args[0]]
+                # Remove it
+                self[config_key.pluralize].delete(args[0])
+                # Insert it back at te right posision
+                self[config_key.pluralize] = Hash[subhash.insert(insert_position, element)]
+            end
+
         end
   
       end
