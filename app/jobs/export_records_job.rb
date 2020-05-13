91c1c65ce7e84c8bb59be77f588a2af8afff4087
Rodolfo Zitellini <rwz@xhero.org>
2020-05-13 17:08:24 +0200

    Test file creation and zipping

--- app/jobs/export_records_job.rb
+++ app/jobs/export_records_job.rb
@@ -1,5 +1,7 @@
 class ExportRecordsJob < ProgressJob::Base
-  
+    
+    MAX_PROCESSES = 10
+
     def initialize(parent_obj_id, parent_obj_class)
       @parent_obj_id = parent_obj_id
       @parent_obj_class = parent_obj_class
@@ -23,35 +25,52 @@ class ExportRecordsJob < ProgressJob::Base
 
         f = Folder.find(@parent_obj_id)
 
- 
-=begin
-        File.open("#{Rails.root}/tmp/test.xml", "w") do |file| 
-            f.folder_items.limit(30000).each do |f|
-                file.write(f.item.marc.to_xml_record(nil, nil, true))
-                update_stage_progress("Exporting #{f.item.id}")
-            end
-        end
-=end
-        # Force a reconnect
-
-
-        limit = f.folder_items.count / 10
+        limit = f.folder_items.count / MAX_PROCESSES
         max = f.folder_items.count
         update_progress_max(max)
-        count = 0
-        Parallel.map(0..10, in_processes: 10) do |jobid|
+
+        # Let's make enough tempfiles
+        tempfiles = []
+        (1..MAX_PROCESSES).each {tempfiles << Tempfile.new('export')}
+
+        Parallel.map(0..MAX_PROCESSES - 1, in_processes: MAX_PROCESSES) do |jobid|
             ActiveRecord::Base.connection.reconnect!
             offset = limit * jobid
-            File.open("#{Rails.root}/tmp/test#{jobid}.xml", "w") do |file|
-                f.folder_items.order(:id).limit(limit).offset(offset).each do |fi|
-                    file.write(fi.item.marc.to_xml_record(nil, nil, true))
+            count = 0
+            #File.open("#{Rails.root}/tmp/test#{jobid}.xml", "w") do |file|
+            f.folder_items.order(:id).limit(limit).offset(offset).each do |fi|
+                tempfiles[jobid].write(fi.item.marc.to_xml_record(nil, nil, true))
+                # We approximante the progress by having only one process write to it
+                if jobid == 0
                     count += 1
-                    update_stage_progress("Updated #{count * 10}/#{max}", step: 200) if count % 20 == 0 && jobid == 0
+                    update_stage_progress("Exported #{count * MAX_PROCESSES}/#{max}", step: 200) if count % 20 == 0 && jobid == 0
                 end
             end
+            #end
+            tempfiles[jobid].rewind
             ActiveRecord::Base.connection.reconnect!
         end
         ActiveRecord::Base.connection.reconnect!
+
+        update_stage("Finalizing export")
+
+        # Now concatenate the files together
+        time = Time.now.strftime('%Y-%m-%d_%H%M%S')
+        filename = "export_#{time}"
+        File.open("#{Rails.root}/public/#{filename}.xml", "w") do |file|
+            tempfiles.each {|tf| file.write(tf.read)}
+        end
+
+        # Compress it so the user soes not faint
+        Zip::File.open("#{Rails.root}/public/#{filename}.zip", Zip::File::CREATE) do |zipfile|
+            zipfile.add(filename + ".xml", "#{Rails.root}/public/#{filename}.xml")
+        end
+          
+        File.unlink("#{Rails.root}/public/#{filename}.xml")
+
+        # Clean up the tempfiles
+        tempfiles.each {|tf| tf.close; tf.unlink}
+
     end
     
     def destroy_failed_jobs?
