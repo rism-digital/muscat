27a6ae9b9b5541fe1ca2fbeacafe15074e474b4c
Rodolfo Zitellini <rwz@xhero.org>
2020-05-20 15:32:52 +0200

    Use conservative locking in delayed job

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,3 +2,7 @@ Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 1.day
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+
+Delayed::Backend::ActiveRecord.configure do |config|
+    config.reserve_sql_strategy = :default_sql # Use the conservative locking
+end
