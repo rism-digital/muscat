2a22423abf078b9d6f0c7758fc4ac652a76c4a3d
Rodolfo Zitellini <rwz@xhero.org>
2020-05-20 15:29:02 +0200

    Better naming, fix url

--- app/mailers/export_ready_notification.rb
+++ app/mailers/export_ready_notification.rb
@@ -6,7 +6,7 @@ class ExportReadyNotification < ApplicationMailer
       
       mail(to: email,
         from: "#{RISM::DEFAULT_EMAIL_NAME} Export <#{RISM::DEFAULT_NOREPLY_EMAIL}>",
-        subject: "Export #{file} ready for download")
+        subject: "File #{file} ready for download")
     end
   
   end
