5607e144493f5c65607800361d56554cc247add8
Rodolfo Zitellini <rwz@xhero.org>
2020-11-23 13:18:16 +0100

    Update configuraition for debug mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.assets.check_precompiled_asset = false
+
 	config.action_mailer.default_url_options = { :host => "muscat.rism.info" }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
