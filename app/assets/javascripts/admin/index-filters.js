f6c5ff3e32711e98dde2942652a54a2b66b04344
Rodolfo Zitellini <rwz@xhero.org>
2020-11-23 13:00:08 +0100

    It does not need to be a const

--- app/assets/javascripts/admin/index-filters.js
+++ app/assets/javascripts/admin/index-filters.js
@@ -2,7 +2,7 @@
 // put this in e.g. app/assets/javascripts/admin/index-filters.coffee
 // and include this in app/assets/javascripts/active_admin.js
  
-const indexFiltersOnLoad = function() {
+indexFiltersOnLoad = function() {
  
   // Modify the clear-filters button to clear saved filters by adding a parameter
   $('.clear_filters_btn').click(() => window.location.search = 'clear_filters=true');
