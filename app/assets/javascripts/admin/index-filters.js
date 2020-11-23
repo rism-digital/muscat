5a68261ffc38e7fae7f0ebbdb1e17f02fb11f494
Rodolfo Zitellini <rwz@xhero.org>
2020-11-23 13:08:16 +0100

    Try to make uglifier happy

--- app/assets/javascripts/admin/index-filters.js
+++ app/assets/javascripts/admin/index-filters.js
@@ -1,15 +1,13 @@
-// This was a coffee file and got "decaffeinated"
 // put this in e.g. app/assets/javascripts/admin/index-filters.coffee
 // and include this in app/assets/javascripts/active_admin.js
  
-indexFiltersOnLoad = function() {
+let indexFiltersOnLoad = function() {
  
   // Modify the clear-filters button to clear saved filters by adding a parameter
-  $('.clear_filters_btn').click({window.location.search = 'clear_filters=true'});
+  $('.clear_filters_btn').click(() => window.location.search = 'clear_filters=true');
 
   // Do the same but for searching in embedded lists
-  $('.embedded_search_btn').click({window.location.search = 'clear_embedded_filters=true'});
-
+  $('.embedded_search_btn').click(() => window.location.search = 'clear_embedded_filters=true');
 };
 	  
 
