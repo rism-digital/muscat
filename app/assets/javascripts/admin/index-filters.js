7554ffb44855ac0a219820594d39a8c416967426
Rodolfo Zitellini <rwz@xhero.org>
2020-11-23 13:11:16 +0100

    Will var make it happy?

--- app/assets/javascripts/admin/index-filters.js
+++ app/assets/javascripts/admin/index-filters.js
@@ -1,8 +1,7 @@
 // put this in e.g. app/assets/javascripts/admin/index-filters.coffee
 // and include this in app/assets/javascripts/active_admin.js
  
-let indexFiltersOnLoad = function() {
- 
+var indexFiltersOnLoad = function() {
   // Modify the clear-filters button to clear saved filters by adding a parameter
   $('.clear_filters_btn').click(() => window.location.search = 'clear_filters=true');
 
