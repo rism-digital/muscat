0cd7deb0661cf21d59af5674e771a88ec9a31bd4
stephan <stephan@hirsch-online.com>
2021-02-18 13:24:18 +0100

    Adding controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -95,6 +95,7 @@ Rails.application.routes.draw do
   get 'sru/institutions' => 'sru#service'
   get 'sru/publications' => 'sru#service'
   get 'sru/catalogues' => 'sru#service'
+  get 'sru/works' => 'sru#service'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
