0bf0ed988a9e7e62f0fdb526745db85b489591f1
Rodolfo Zitellini <rwz@xhero.org>
2021-06-29 14:01:45 +0200

    #1124 Fix "inprogress" and "unpublished" legacy values in versions

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -35,7 +35,8 @@ class Holding < ApplicationRecord
   attr_accessor :suppress_update_count_trigger
   attr_accessor :suppress_update_77x_trigger
 
-  enum wf_stage: [ :inprogress, :published, :deleted, :deprecated ]
+  # Keep both inprogress and unpublished for compatibility with older versions
+  enum wf_stage: { unpublished: 0, inprogress: 0, published: 1, deleted: 2, deprecated: 3 }
   enum wf_audit: [ :unapproved, :full, :abbreviated, :retro, :imported ]
 
   def after_initialize
