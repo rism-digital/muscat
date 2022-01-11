f1b602d4a35974eccc8385510d3d9840a23961f4
Rodolfo Zitellini <rwz@xhero.org>
2022-01-11 12:51:50 +0100

    Add note for MailCatcher gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  # For use with the MailCatcher gem, very handy to test emails sent by muscat  
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
