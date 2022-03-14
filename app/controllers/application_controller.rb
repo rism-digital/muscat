1d59cc78db6384a3d4188b509546a5251d4acf2f
Rodolfo Zitellini <rwz@xhero.org>
2022-03-14 13:17:40 +0100

    New fix for redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,18 +13,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
+  before_action :store_user_location!, if: :storable_location?
   before_action :set_locale, :set_paper_trail_whodunnit, :auth_user, :prepare_exception_notifier, :test_version_warning, :test_muscat_reindexing
-  after_action :store_location
 
+  # see https://github.com/heartcombo/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
 
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (request.fullpath != "/admin/login" &&
-        request.fullpath != "/admin/logout" &&
-        !request.xhr?) # don't store ajax calls
-      session["user_return_to"] = request.fullpath 
-    end
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 
   def prepare_exception_notifier
