bfc8e8855fcd5cf9a36c3e5c5f366a6be760164b
Laurent Pugin <lxpugin@gmail.com>
2022-06-17 11:05:36 +0200

    Adjust MarcWorkNode class
    * Change class name
    * Revert changes from the work experiments in 7702657fa54f686428390ca387df523973028179

--- lib/marc_work_node.rb
+++ lib/marc_work_node.rb
@@ -1,6 +1,6 @@
-class MarcWork < Marc
+class MarcWorkNode < Marc
   def initialize(source = nil)
-    super("work", source)
+    super("work_node", source)
   end
   
   def get_title
@@ -11,37 +11,18 @@ class MarcWork < Marc
     if node = first_occurance("100", "t")
       title = node.content.blank? ? " [without title]" : " #{node.content}"
     end
-    #if node = first_occurance("100", "n")
-    #  number = node.content.blank? ? "" : " #{node.content}"
-    #end
+    if node = first_occurance("100", "n")
+      number = node.content.blank? ? "" : " #{node.content}"
+    end
     if node = first_occurance("100", "r")
       key = node.content.blank? ? "" : " #{node.content}"
     end
-    #if node = first_occurance("100", "m")
-    #  scoring = node.content.blank? ? "" : "; #{node.content}"
-    #end
+    if node = first_occurance("100", "m")
+      scoring = node.content.blank? ? "" : "; #{node.content}"
+    end
 
-    return "#{composer}#{title}#{key}"
+    return "#{composer}#{title}#{number}#{key}#{scoring}"
   end
-
-  def get_form
-    node = first_occurance("383", "b")
-    opus = node.content.truncate(50) if node && node.content
-    opus = opus ? opus.strip : ""
-  end
-
-  def get_notes
-    node = first_occurance("690", "a")
-    cat_a = node.content.truncate(50) if node && node.content
-    cat_a = cat_a.strip if cat_a
-    
-    node = first_occurance("690", "n")
-    cat_n = node.content.truncate(50) if node && node.content
-    cat_n = cat_n.strip if cat_n
-   
-    cat_no = "#{cat_a} #{cat_n}".strip
-    cat_no = cat_no.empty? ? "" : cat_no
-  end 
 
   def get_composer
     if node = first_occurance("100", "a")
@@ -49,7 +30,5 @@ class MarcWork < Marc
     end
     return person
   end
-
-
 
 end
