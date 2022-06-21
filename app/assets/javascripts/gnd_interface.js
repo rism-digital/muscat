7447db82d5a1aa28956edf1b8fb51e166e6f04f3
Laurent Pugin <lxpugin@gmail.com>
2022-06-21 16:55:59 +0200

    Tidying up JS gnd interface
    * Fix update of 100a

--- app/assets/javascripts/gnd_interface.js
+++ app/assets/javascripts/gnd_interface.js
@@ -7,7 +7,7 @@ var show_gnd_actions = function () {
   });
 
   $gnd_table.on('click', '.data', function() {
-    _update_form($(this).data("gnd"));
+    _update_form_gnd($(this).data("gnd"));
     marc_editor_show_panel("marc_editor_panel");
   });
 
@@ -17,7 +17,7 @@ var show_gnd_actions = function () {
   * else: add other tags (new and append)
   * never update fields if not new
   */
-  function _update_form(data){
+  function _update_form_gnd(data){
     protected_fields = ['100']
     tags = data["fields"]
     tag = ""
@@ -33,28 +33,28 @@ var show_gnd_actions = function () {
       }
       if (!($.inArray(datafield.tag, protected_fields))){
         if (/\/new#$/.test(self.location.href)){
-          _update_marc_tag(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
+          _update_marc_tag_gnd(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
         }
         else{
           continue
         }
         continue
       }
-      if (_size_of_marc_tag(datafield.tag) == 0){
+      if (_size_of_marc_tag_gnd(datafield.tag) == 0){
         _new_marc_tag(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
       }
       else{
-        if (_marc_tag_is_empty(datafield.tag)){
-          _update_marc_tag(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
+        if (_marc_tag_is_empty_gnd(datafield.tag)){
+          _update_marc_tag_gnd(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
         }
         else{
-          _append_marc_tag(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
+          _append_marc_tag_gnd(datafield.tag, marc_json_get_tags(data, datafield.tag)[cnt])
         }
       }
     }
   }
 
-  function searchGnd(){
+  function search_gnd(){
     $gnd_table.html("");
     var term = $("#gnd_input").val();
     var model = $("#marc_editor_panel").attr("data-editor-model");
@@ -72,29 +72,29 @@ var show_gnd_actions = function () {
         },
         success: function(data){
           var result = (JSON.stringify(data));
-          drawTable(data);
+          draw_table_gnd(data);
         }
       });
   }
 
   $("#gnd_button").click(function(){ 
-    searchGnd()
+    search_gnd()
   })
 
   $('#gnd_input').keydown(function (e) {
     var keyCode = e.keyCode || e.which;
     if (keyCode == 13) { 
-      searchGnd()
+      search_gnd()
     }
   });
 
-  function drawTable(data) {
+  function draw_table_gnd(data) {
     for (var i = 0; i < data.length; i++) {
-      drawRow(data[i]);
+      draw_row_gnd(data[i]);
     }
   }
 
-  function drawRow(rowData) {
+  function draw_row_gnd(rowData) {
     locale = $gnd_table.attr("locale")
     message = {"de": "übernehmen", "en": "select", "fr": "choisir", "it": "scegliere"}[locale]
     var id = marc_json_get_tags(rowData, "001")[0].content;
@@ -115,16 +115,14 @@ var show_gnd_actions = function () {
   }
 };
 
-function _update_marc_tag(target, data) {
+function _update_marc_tag_gnd(target, data) {
   block = $(".marc_editor_tag_block[data-tag='" + target + "']")
   var model = $("#marc_editor_panel").attr("data-editor-model");
+  console.log(data, target)
   for (code in data){
     subfield = block.find(".subfield_entry[data-tag='" + target + "'][data-subfield='" + code + "']").first()
     if (model == "work_nodes" && target == "100" && code == "a") {
-      subfield = $("#100a");
-    }
-    if (model == "work_nodes" && target == "100" && code == "0") {
-      subfield = $("input[data-tag='100'][data-subfield='0']");
+      subfield = block.find("input[data-field='" + target + "'][data-subfield='" + code + "']").first()
     }
     subfield.val(data[code]);
     subfield.css("background-color", "#ffffb3");
@@ -147,7 +145,7 @@ function _new_marc_tag(target, data) {
   new_dt.parents(".tag_group").children(".tag_empty_container").hide();
 }
 
-function _append_marc_tag(target, data) {
+function _append_marc_tag_gnd(target, data) {
   block = $(".marc_editor_tag_block[data-tag='" + target + "']")
   placeholder = block.parents(".tag_group").children(".tag_placeholders_toplevel").children(".tag_placeholders");
   new_dt = placeholder.clone()
@@ -161,12 +159,12 @@ function _append_marc_tag(target, data) {
   new_dt.show()
 }
 
-function _size_of_marc_tag(tag){
+function _size_of_marc_tag_gnd(tag){
   fields = $(".tag_toplevel_container[data-tag='"+ tag +"']")
   return fields.length
 }
 
-function _marc_tag_is_empty(tag){
+function _marc_tag_is_empty_gnd(tag){
   block = $(".marc_editor_tag_block[data-tag='" + tag + "']")
   subfields = block.find("input.subfield_entry[data-tag='" + tag + "']")
   for (var i = 0; i < subfields.length; i++){
