9b1619451b701d045b3e6fd15c1e217389ce0f6e
Rodolfo Zitellini <rwz@xhero.org>
2022-09-26 16:20:33 +0200

    Try using flash

--- app/admin/source.rb
+++ app/admin/source.rb
@@ -108,7 +108,7 @@ ActiveAdmin.register Source do
       # We get here before ActiveAdmin::AccessDenied is thrown
       # So we can redirect gracefully to edit
       if cannot?(:edit, @item)
-        redirect_to admin_source_path(@item), :warning => "Unauthorized"
+        redirect_to admin_source_path(@item), :flash => { :error => "ERROR" }
       end
 
       # Try to load the MARC object.
