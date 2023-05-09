57c3e8d09916fa88dc53816ae22320fed18ac2f9
Ferran Jorba <Ferran.Jorba@gmail.com>
2023-05-09 16:12:50 +0200

    Allow import_from_marc to import other Marc types, and optional arguments
    As many record types in Muscat are Marc based, expand the importing script
    so it can import both bibliographic records (Sources and Publications), but
    also authority records for people and institutions.
    
    Allow also to optionally select first and last record, create (scaffold)
    authority records when needed, real-time Solr indexing and versioning.
    
    Closes #1157 and #601 (when records are in MarcXML).

--- housekeeping/import/import_from_marc.rb
+++ housekeeping/import/import_from_marc.rb
@@ -1,38 +1,34 @@
 # Import Marcxml records into Muscat database
 # Run as bin/rails runner housekeeping/import/import_from_marc.rb
 
-VALID_MODELS = ["Source", "Publication", "Holding", "Person", "Institution",
-                "Work"]
+VALID_MODELS = ["Source", "Publication", "Person", "Institution"]
 
 def display_help
   script = "bin/rails runner #{$0}"
-  valid_models_list = VALID_MODELS.join(', ')
   puts "Import marcxml records into Muscat database
 
 Usage:  #{script} [options]
 
 Options:
- -f, --file        input file name, required
- -t, --type        record type; choose one of: #{valid_models_list}
- -m, --from        first record to import
- -v, --versioning  update records version
- -u, --authorities create (scaffold) related Marc authorities records
- -i, --insert      only add new records, skip duplicates [NOT YET IMPLEMENTED]
- -r, --replace     only overwrite existing records, skip new ones [NOT YET]
- -a, --append      only append non-existing tags to existing records [NOT YET]
- -n, --dry-run     only simulate, do not update database
- -h, --help        this help
+ -i, --input, --file input file name, required
+ -t, --type          record type; choose one of: #{VALID_MODELS.join(', ')}
+ -f, --first         first record to import
+ -l, --last          last record to import
+ -v, --versioning    update records version
+ -u, --authorities   create (scaffold) related Marc authorities records
+ -x, --index         index records as they are imported
+ -h, --help          this help
 
 This script can also be run with positional arguments:
 
- #{script} filename type [from]
+ #{script} filename type [first]
 "
   exit false
 end
 
 
 runs_under_rails = Rails.initialized? rescue nil
-if ! runs_under_rails then
+if !runs_under_rails
   $stderr.puts "This script has to be run under Rails"
   display_help
 end
@@ -40,21 +36,19 @@ end
 source_file = nil
 model = nil
 options = {
-  from: 0,
+  first: 0,
+  last: 9999999,
   versioning: false,
-  scaffold: false,
-  insert: false,
-  replace: false,
-  append: false,
-  dry_run: false,
+  authorities: false,
+  index: false,
 }
 
 while ARGV.any? do
   arg = ARGV.shift
-  if ["-f", "--file"].include? arg
-    if ARGV.any? then
+  if ["-i", "--input", "--file"].include? arg
+    if ARGV.any?
       arg = ARGV.shift
-      if File.exists? arg then
+      if File.exists? arg
         source_file = arg
       else
         $stderr.puts "#{arg} is not a file!"
@@ -62,54 +56,64 @@ while ARGV.any? do
       end
     end
   elsif ["-t", "--type"].include? arg
-    if ARGV.any? then
+    if ARGV.any?
       arg = ARGV.shift
       if VALID_MODELS.include? arg
         model = arg
       else
-        $stderr.puts "Invalid record type: #{arg}\nValid values are: " + VALID_MODELS.join(', ')
+        $stderr.puts "Invalid record type: #{arg}\nValid values are: #{VALID_MODELS.join(', ')}"
         exit false
       end
     end
-  elsif ["-m", "--from"].include? arg
-    if ARGV.any? then
+  elsif ["-f", "--first"].include? arg
+    if ARGV.any?
       arg = ARGV.shift
       if Integer(arg, exception: false)
-        options[:from] = arg.to_i
+        options[:first] = arg.to_i
       else
         $stderr.puts "Non-numeric argument: #{arg}"
         exit false
       end
+    else
+      $stderr.puts "Missing numeric argument: #{arg}"
+      exit false
+    end
+  elsif ["-l", "--last"].include? arg
+    if ARGV.any?
+      arg = ARGV.shift
+      if Integer(arg, exception: false)
+        options[:last] = arg.to_i
+      else
+        $stderr.puts "Non-numeric argument: #{arg}"
+        exit false
+      end
+    else
+      $stderr.puts "Missing numeric argument: #{arg}"
+      exit false
     end
   elsif ["-v", "--versioning"].include? arg
     options[:versioning] = true
-  elsif ["-a", "--authorities"].include? arg
+  elsif ["-u", "--authorities"].include? arg
     options[:authorities] = true
-  elsif ["-r", "--replace"].include? arg
-    options[:replace] = true
-  elsif ["-i", "--insert"].include? arg
-    options[:insert] = true
-  elsif ["-a", "--append"].include? arg
-    options[:append] = true
-  elsif ["-n", "--dry-run"].include? arg
-    options[:dry_run] = true
+  elsif ["-x", "--index"].include? arg
+    options[:index] = true
   elsif ["-h", "--help"].include? arg
     display_help
   # The following options are for backward compatibility
-  elsif File.exists? arg then
+  elsif File.exists? arg
     source_file = arg
   elsif VALID_MODELS.include? arg
     model = arg
   elsif Integer(arg, exception: false)
     options[:from] = arg.to_i
   else
-    $stderr.puts "Unrecognised option: #{arg}"
+    $stderr.puts "Unrecognized option: #{arg}"
     display_help
   end
 end
 
-if source_file && model then
-  if ! versioning
+if source_file && model
+  if !options[:versioning]
     VALID_MODELS.each do |model|
       PaperTrail.request.disable_model(model.constantize)
     end
--- lib/marc_import.rb
+++ lib/marc_import.rb
@@ -1,20 +1,20 @@
 # Reads from a MARCXML file and imports the records into the database
 
 require 'nokogiri'
-require 'logger' 
+require 'logger'
 
 class MarcImport
-  
+
   def initialize(source_file, model, options)
     #@log = Logger.new(Rails.root.join('log/', 'import.log'), 'daily')
     @source_file = source_file
     @model = model
     @options = options
-    @total_records = open(source_file) { |f| f.grep(/001">/) }.size
+    @total_records = open(source_file) { |f| f.grep(/<record>/) }.size
     @import_results = Array.new
     @cnt = 0
     @start_time = Time.now
-    
+
     # Load users, for source model
     @users = nil
     if File.exist?("link_users.yml") && @model == "Source"
@@ -23,7 +23,7 @@ class MarcImport
     else
       puts "No User file provided"
     end
-    
+
     MarcConfigCache.get_configuration model.downcase
     MarcConfigCache.add_overlay(model, "#{Rails.root}/housekeeping/import/import_tags_source.yml") if @model == "Source"
   end
@@ -40,163 +40,160 @@ class MarcImport
   end
 
   def import
-    each_record(@source_file) { |record|         
+    n = 0
+    each_record(@source_file) do |record|
+      n += 1
+      if n >= @options[:first] && n <= @options[:last]
         rec = Nokogiri::XML(record.to_s)
         # Use external XSLT 1.0 file for converting to MARC21 text
         xslt  = Nokogiri::XSLT(File.read(Rails.root.join('housekeeping/import/', 'marcxml2marctxt_1.0.xsl')))
         marctext = CGI::unescapeHTML(xslt.transform(rec).to_s)
         create_record(marctext)
-    }
+      end
+    end
     puts @import_results
   end
 
   def create_record(buffer)
     @cnt += 1
-    #@total_records += 1
     buffer.gsub!(/[\r\n]+/, ' ')
     buffer.gsub!(/ (=[0-9]{3,3})/, "\n\\1")
-    if @total_records >= @options[:from]
-      marc = Object.const_get("Marc" + @model).new(buffer)
-      # load the text source but without resolving externals
-      marc.load_source(false)
-      
-      if marc.is_valid?(false)
-       
-        # step 1.  update or create a new object
-        model = Object.const_get(@model).find_by_id(marc.get_id)
-        if !model
-          status = "created"
-          if @model == "Publication"
-            model = Object.const_get(@model).new(:id => marc.get_id, :name => marc.get_name, :author => marc.get_author, :revue_title=> marc.get_revue_title, :description => marc.get_description, :wf_owner => 1, :wf_stage => "published")
-          elsif @model == "Source"
-            model = Object.const_get(@model).new(:id => marc.get_id, :lib_siglum => marc.get_siglum, :wf_owner => 1, :wf_stage => "published")
-          else
-            model = Object.const_get(@model).new(:id => marc.get_id, :wf_owner => 1, :wf_stage => "published")
-          end
-        else
-          status = "updated"
-        end
-        
-        moddate = marc.first_occurance('005')
-        if moddate && moddate.content
-          begin
-            date = DateTime.parse(moddate.content)
-            updated_at = date if date
-          rescue => e
-            $stderr.puts "Cannot parse date for #{model.id}, #{moddate.content} #{e.message}"
-          end
-        end
+    marc = Object.const_get("Marc" + @model).new(buffer)
+    # load the text source but without resolving externals
+    marc.load_source(false)
 
-        createdate = marc.first_occurance('008')
-        if createdate && createdate.content
-          begin
-            date = DateTime.parse(createdate.content[0, 6])
-            created_at = date if date
-          rescue => e
-            $stderr.puts "Cannot parse date for #{model.id}, #{createdate.content} #{e.message}"
-          end
+    if marc.is_valid?(false)
+
+      # step 1.  update or create a new object
+      model = Object.const_get(@model).find_by_id(marc.get_id)
+      if !model
+        status = "created"
+        if @model == "Publication"
+          model = Object.const_get(@model).new(:id => marc.get_id, :name => marc.get_name, :author => marc.get_author, :journal=> marc.get_journal, :title => marc.get_title, :wf_owner => 1, :wf_stage => "published")
+        elsif @model == "Source"
+          model = Object.const_get(@model).new(:id => marc.get_id, :lib_siglum => marc.get_siglum, :wf_owner => 1, :wf_stage => "published")
+        else
+          model = Object.const_get(@model).new(:id => marc.get_id, :wf_owner => 1, :wf_stage => "published")
         end
-        
-        if updated_at && created_at
-          if created_at < updated_at
+      else
+        status = "updated"
+      end
+
+      moddate = marc.first_occurance('005')
+      if moddate && moddate.content
+        begin
+          date = DateTime.parse(moddate.content)
+          updated_at = date if date
+        rescue => e
+          $stderr.puts "Cannot parse date for #{model.id}, #{moddate.content} #{e.message}"
+        end
+      end
+
+      createdate = marc.first_occurance('008')
+      if createdate && createdate.content
+        begin
+          date = DateTime.parse(createdate.content[0, 6])
+          created_at = date if date
+        rescue => e
+          $stderr.puts "Cannot parse date for #{model.id}, #{createdate.content} #{e.message}"
+        end
+      end
+
+      if updated_at && created_at
+        if created_at < updated_at
+          model.created_at = created_at
+          model.updated_at = updated_at
+        else
+          $stderr.puts "created_at > updated_at #{created_at.to_s}, #{updated_at.to_s}, #{model.id}"
+          model.created_at = created_at
+          model.updated_at = created_at
+        end
+      elsif updated_at && !created_at ## the missing value will be date of import
+        #$stderr.puts "No created_at for #{model.id}"
+        model.updated_at = updated_at
+      elsif !updated_at && created_at
+        #$stderr.puts "No updated_at for #{model.id}"
+        model.created_at = created_at
+      else
+        #$stderr.puts "No date information for #{model.id}"
+      end
+
+      # Make internal format
+      marc.to_internal
+
+      # step 2. do all the lookups and change marc fields to point to external entities (where applicable)
+      marc.suppress_scaffold_links unless @options[:authorities]
+      marc.import
+
+      # step 3 resolve external values if it is a source or publication
+      #begin
+      if ["Source", "Publication"].include? @model
+        marc.root.resolve_externals if @options[:authorities]
+      end
+      #rescue => e
+      #$stderr.puts
+      #$stderr.puts "Marc Import: Could not resolve externals on record".red
+      #$stderr.puts e.message.blue
+      #$stderr.puts "Record Id: #{model.id}".magenta
+      #$stderr.puts "#{marc.to_marc}"
+      #end
+
+      # step 4. associate Marc to record
+      model.marc = marc
+      @import_results.concat( marc.results )
+      @import_results = @import_results.uniq
+
+      if @model == "Source"
+        model.suppress_update_77x # we should not need to update the 774/773 relationships during the import
+        model.suppress_update_count # Do not update the count for the foreign objects
+        rt = marc.record_type
+        if (rt)
+          model.record_type = rt
+        else
+          "Empty record type for #{s.id}"
+        end
+      end
+
+      model.suppress_reindex unless @options[:index]
+
+      # In institution postpone the workgroup link creation
+      model.suppress_update_workgroups if model.respond_to? :suppress_update_workgroups
+
+      # Add user if exists, only for sources
+      if @users && @model == "Source"
+        if @users.include?(model.id.to_s)
+          name = @users[model.id.to_s][:user]
+          created_at = DateTime.parse(@users[model.id.to_s][:created_at])
+          updated_at = DateTime.parse(@users[model.id.to_s][:updated_at])
+          begin
+            user = User.find_by_name(name)
+            model.user = user
             model.created_at = created_at
             model.updated_at = updated_at
-          else
-            $stderr.puts "created_at > updated_at #{created_at.to_s}, #{updated_at.to_s}, #{model.id}"
-            model.created_at = created_at
-            model.updated_at = created_at
-          end
-        elsif updated_at && !created_at ## the missing value will be date of import
-          #$stderr.puts "No created_at for #{model.id}"
-          model.updated_at = updated_at
-        elsif !updated_at && created_at
-          #$stderr.puts "No updated_at for #{model.id}"
-          model.created_at = created_at
-        else
-          #$stderr.puts "No date information for #{model.id}"
-        end
-        
-        # Make internal format
-        marc.to_internal
-
-        # step 2. do all the lookups and change marc fields to point to external entities (where applicable)
-        if ! @options[:scaffold]
-          marc.suppress_scaffold_links
-          marc.import
-        end
-        
-        # step 3 resolve external values if it is a source
-        #begin
-          marc.root.resolve_externals if @model == "Source"
-					#rescue => e
-          #$stderr.puts
-          #$stderr.puts "Marc Import: Could not resolve externals on record".red
-          #$stderr.puts e.message.blue
-          #$stderr.puts "Record Id: #{model.id}".magenta
-          #$stderr.puts "#{marc.to_marc}"
-					#end
-        
-        # step 4. associate Marc to record
-        model.marc = marc
-        @import_results.concat( marc.results )
-        @import_results = @import_results.uniq
-
-        if @model == "Source"
-          model.suppress_update_77x # we should not need to update the 774/773 relationships during the import
-          model.suppress_update_count # Do not update the count for the foreign objects
-          rt = marc.record_type
-          if (rt)
-            model.record_type = rt
-          else
-            "Empty record type for #{s.id}"
+          rescue ActiveRecord::RecordNotFound
+            puts "Could not find user #{name}".red
           end
         end
-        
-        model.suppress_reindex
-        
-        # In institution postpone the workgroup link creation
-        model.suppress_update_workgroups if model.respond_to? :suppress_update_workgroups
-        
-        # Add user if exists, only for sources
-        if @users && @model == "Source"
-          if @users.include?(model.id.to_s)
-            name = @users[model.id.to_s][:user]
-            created_at = DateTime.parse(@users[model.id.to_s][:created_at])
-            updated_at = DateTime.parse(@users[model.id.to_s][:updated_at])
-            begin
-              user = User.find_by_name(name)
-              model.user = user
-              model.created_at = created_at
-              model.updated_at = updated_at
-            rescue ActiveRecord::RecordNotFound
-              puts "Could not find user #{name}".red
-            end
-          end
-        end
-        
-         # step 4. insert model into database
-        #begin
-        if ! @options[:dry_run]
-          model.save! #
-        end
-#          @log.info(@model+" record "+marc.get_id.to_s+" "+status)
-#        rescue ActiveRecord::RecordNotUnique
-#          @log.error(@model+" record "+marc.get_id.to_s+" import failed because record not unique")
-        #rescue => e
-          #$stderr.puts
-          #$stderr.puts "Marc Import: Could not save the imported record".red
-          #$stderr.puts e.message.blue
-          #$stderr.puts "Record Id: #{model.id}".magenta
-          #$stderr.puts "#{marc.to_marc}"
-          #puts e.backtrace.join("\n")
-					#end
-        print "\rStarted: " + @start_time.strftime("%Y-%m-%d %H:%M:%S").green + " -- Record #{@cnt} of #{@total_records} processed".yellow
-        #puts "Last offset: #{@total_records}, Last "+@model+" RISM ID: #{marc.first_occurance('001').content}"
-      else
-        $stderr.puts "Marc is not valid! #{buffer}"
       end
+
+      # step 4. insert model into database
+#     begin
+      model.save! #
+#       @log.info(@model+" record "+marc.get_id.to_s+" "+status)
+#       rescue ActiveRecord::RecordNotUnique
+#       @log.error(@model+" record "+marc.get_id.to_s+" import failed because record not unique")
+#       rescue => e
+#        $stderr.puts
+#        $stderr.puts "Marc Import: Could not save the imported record".red
+#        $stderr.puts e.message.blue
+#        $stderr.puts "Record Id: #{model.id}".magenta
+#        $stderr.puts "#{marc.to_marc}"
+#        puts e.backtrace.join("\n")
+#        end
+      print "\rStarted: " + @start_time.strftime("%Y-%m-%d %H:%M:%S").green + " -- Record #{@cnt} of #{@total_records} processed".yellow
+    #puts "Last offset: #{@total_records}, Last "+@model+" RISM ID: #{marc.first_occurance('001').content}"
+    else
+      $stderr.puts "Marc is not valid! #{buffer}"
     end
   end
 end
-
-
