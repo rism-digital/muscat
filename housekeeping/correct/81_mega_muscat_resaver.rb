da8b40d830ea127b09b9c40faf18992c183c7360
Rodolfo Zitellini <rwz@xhero.org>
2024-01-25 13:08:42 +0100

    Add some eye-candy to the resave megascript

--- housekeeping/correct/81_mega_muscat_resaver.rb
+++ housekeeping/correct/81_mega_muscat_resaver.rb
@@ -6,20 +6,27 @@ begin_time = Time.now
 all_items = 0
 all_unsaved = 0
 
+spinner = TTY::Spinner.new("[:spinner] :title", format: :shark)
+
 models.each do |model|
     begin_model_time = Time.now
     total_items = model.all.count
+    saved_items = 0 #With find_in_batches each_with_index does not work!
     unsavable_items = 0
     
     puts "Resaving #{total_items} #{model.to_s.pluralize}"
-    
+    spinner.update(title: "Loading #{model.to_s.pluralize}...")
+    spinner.auto_spin
+
     model.find_in_batches do |batch|
-        batch.each_with_index do |item, idx|
+        batch.each do |item|
+            spinner.update(title: "#{model.to_s} offset: #{saved_items} ") if saved_items % 1000 == 0
 
             begin
                 # Do not make a paper trail snapshot
                 PaperTrail.request(enabled: false) do
                     item.save
+                    saved_items += 1
                 end
             rescue
                 puts "Could not save #{model.to_s} #{item.id}"
@@ -27,7 +34,7 @@ models.each do |model|
                 all_unsaved += 1
             end
 
-            $stderr.print "#{model.to_s[0, 3].upcase}#{idx} " if DOTIFY && idx % 1000 == 0
+            #$stderr.print "#{model.to_s[0, 3].upcase}#{idx} " if DOTIFY && idx % 1000 == 0
 
             item = nil
         end
@@ -36,7 +43,7 @@ models.each do |model|
     end_model_time = Time.now
     all_items += total_items - unsavable_items
     puts "Saved #{total_items - unsavable_items} #{model.to_s.pluralize} in #{end_model_time - begin_model_time}"
-    
+
 end
 
 end_time = Time.now
