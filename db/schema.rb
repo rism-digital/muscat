ebee9d2f5bfdcd2a17afa006af6d81cf491bb4ec
Rodolfo Zitellini <rwz@xhero.org>
2024-05-27 14:16:39 +0200

    Move ISO country codes to a shared file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_14_090418) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_21_090418) do
   create_table "active_admin_comments", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
