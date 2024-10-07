d569b3e8b026ef6318232c5e3cebc0f2e5871f63
Rodolfo Zitellini <rwz@xhero.org>
2024-10-07 11:14:14 +0200

    De-pikadize with ease!

--- housekeeping/scrapbook/pikado_parser.rb
+++ housekeeping/scrapbook/pikado_parser.rb
@@ -1,37 +1,60 @@
 require "awesome_print"
 
-FILENAME="TIT.ASC"
+FILENAME="/Users/xhero/Downloads/TIT.ASC"
 
 def parse_records(filename)
-    records = {}
-    current_record = nil
-    current_id = nil
-  
-    File.open(filename, "r:ISO-8859-1") do |file|
-      file.each_line do |line|
-        line = line.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?').chomp
-  
-        if line.start_with?('###')
-            current_record = {}
-          current_id = line[3..-1].to_i
-          records[current_id] = current_record unless current_id.nil?
-        elsif current_record && !line.empty?
-          if line =~ /^(\d{3})(.*)/
-            tag = $1
-            value = $2.strip
-  
-            current_record[tag] ||= []
-            current_record[tag] << value
-          end
+  records = {}
+  current_record = nil
+  current_id = nil
+  spinner = TTY::Spinner.new("[:spinner] :title", format: :shark)
+
+  pb = ProgressBar.new(643976)
+
+  File.open(filename, "r:ISO-8859-1") do |file|
+    file.each_line do |line|
+      line = line.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?').chomp
+
+      if line.start_with?('###')
+        current_record = {}
+        current_id = line[3..-1].to_i
+        records[current_id] = current_record unless current_id.nil?
+        pb.increment!
+      elsif current_record && !line.empty?
+        if line =~ /^(\d{3})(.*)/
+          tag = $1
+          value = $2.strip
+
+          current_record[tag] ||= []
+          current_record[tag] << value
         end
       end
     end
-  
-    records
   end
+  
+  records
+end
     
-  records = parse_records(FILENAME)
-    
+records = parse_records(FILENAME)
+
+# Dump all the records to a semblance of marc
+File.open("pikado2marc.txt", "w") do |file|
+
+  records.each do |record|
+    file.write "\n"
+    file.write "=LDR  04289ndmaa2200493uc 4500\n"
+    file.write "=001  #{record[0]}\n"
+    record[1].each do |tag, values|
+      values.each do |value|
+        file.write "=#{tag}  \\\\$a#{value}\n"
+      end
+    end
+  end
+
+end
+
+
+=begin
+  # do things
   File.open("bad-518.txt") do |file|
     file.each_line do |line|
         record = records[line.strip.to_i]
@@ -45,3 +68,4 @@ def parse_records(filename)
         puts "#{line.strip.to_i}\t#{record['944']}\t#{record['962']}"
     end
   end
+=end
