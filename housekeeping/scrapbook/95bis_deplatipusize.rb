794ff6775d64e133c0cf7a703ae1016cc0d37a57
Rodolfo Zitellini <rwz@xhero.org>
2024-10-15 12:28:12 +0200

    Script to fix the Protypens

---
+++ housekeeping/scrapbook/95bis_deplatipusize.rb
@@ -0,0 +1,18 @@
+File.open("housekeeping/scrapbook/protypen_fix.txt") do |f|
+    f.each_line do |line|
+        id, tag, thing, word, correction, more_correction = line.split("\t")
+
+        if word == correction && more_correction == "\n"
+            puts word
+            next
+        end
+
+        correction = more_correction if more_correction != "\n"
+
+        s = Source.find(id)
+        s.marc_source.gsub!(word, correction)
+        s.paper_trail_event = "Fix protypen #{word} with #{correction}"
+        s.save
+    end
+
+end
