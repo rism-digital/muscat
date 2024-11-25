d811baf9cd2f43dbae5c05928c10997350268e73
Rodolfo Zitellini <rwz@xhero.org>
2024-11-25 14:30:34 +0100

    #1675 swap country and district in the list view

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -117,8 +117,8 @@ ActiveAdmin.register Place do
     selectable_column if !is_selection_mode?
     column (I18n.t :filter_id), :id  
     column (I18n.t :filter_name), :name
-    column (I18n.t :filter_country), :country
     column (I18n.t :filter_district), :district
+    column (I18n.t :filter_country), :country
     column (I18n.t :filter_sources), :src_count_order, sortable: :src_count_order do |element|
 			all_hits = @arbre_context.assigns[:hits]
 			active_admin_stored_from_hits(all_hits, element, :src_count_order)
