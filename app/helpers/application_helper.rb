921fa52f2f80cb4f2fe172d84a21a1db4d682c26
Rodolfo Zitellini <rwz@xhero.org>
2024-12-17 09:35:59 +0100

    Fix #1618, use built-in  Anchored::Linker.auto_link instead of old URL helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -83,20 +83,6 @@ module ApplicationHelper
   def inventory_item_default_autocomplete
   end
 
-  # Create a link for a page in a new window
-  def application_helper_link_http(value, node)
-    result = []
-    links = value.split("\n")
-    links.each do |link|
-      if link.match /(.*)(http:\/\/)([^\s]*)(.*)/
-        result << "#{$1}<a href=\"#{$2}#{$3}\" target=\"_blank\">#{$3}</a>#{$4}"
-      else
-        result << link
-      end
-    end
-    result.join("<br>")
-  end
-  
   # Link a manuscript by its RISM id
   def application_helper_link_source_id(value, subfield) # This could have never worked
       link_to( value, { :action => "show", :controller => "admin/sources", :id => value })
