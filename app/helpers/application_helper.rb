4f5ca992b40363bc2288afac0c7fc1b23f72d85c
Rodolfo Zitellini <rwz@xhero.org>
2025-02-11 11:23:50 +0100

    #1272. add composite_volume for libretto_source and theoretica_source

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -131,7 +131,7 @@ module ApplicationHelper
     elsif source.record_type == MarcSource::RECORD_TYPES[:edition_content]
       MarcSource::RECORD_TYPES[:edition]
     elsif source.record_type == MarcSource::RECORD_TYPES[:libretto_source] || source.record_type == MarcSource::RECORD_TYPES[:theoretica_source]
-      MarcSource::RECORD_TYPES[:collection]
+      [MarcSource::RECORD_TYPES[:collection], MarcSource::RECORD_TYPES[:composite_volume]]
     elsif source.record_type == MarcSource::RECORD_TYPES[:libretto_edition_content]
       MarcSource::RECORD_TYPES[:libretto_edition]
     elsif source.record_type == MarcSource::RECORD_TYPES[:theoretica_edition_content]
