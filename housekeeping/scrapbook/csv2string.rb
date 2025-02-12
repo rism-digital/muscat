c8f3c7596ddec383f66949bb5045b9774d9a2cb2
Rodolfo Zitellini <rwz@xhero.org>
2025-02-12 09:59:53 +0100

    Add scripts to fix 588

---
+++ housekeeping/scrapbook/csv2string.rb
@@ -0,0 +1,27 @@
+require 'csv'
+require 'yaml'
+require 'zlib'
+require 'base64'
+
+def csv_to_base64_gzipped_yaml(csv_file_path)
+  # 1. Read the CSV file and build a hash
+  data_hash = {}
+  CSV.foreach(csv_file_path, headers: false) do |row|
+    key, value = row
+    data_hash[key] = value
+  end
+
+  # 2. Convert the hash to a YAML string
+  yaml_str = data_hash.to_yaml
+
+  # 3. Gzip the YAML string
+  compressed = Zlib::Deflate.deflate(yaml_str)
+
+  # 4. Base64-encode the compressed data
+  Base64.strict_encode64(compressed)
+end
+
+base64_string = csv_to_base64_gzipped_yaml(ARGV[0])
+File.open("output.txt", "w") do |file|
+    file.write base64_string
+end
