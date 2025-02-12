0e1881c07d6254c797569ea035139705184b92f6
Rodolfo Zitellini <rwz@xhero.org>
2025-02-12 13:45:06 +0100

    Switch to array so duplicate entries work

--- housekeeping/scrapbook/csv2string.rb
+++ housekeeping/scrapbook/csv2string.rb
@@ -5,21 +5,22 @@ require 'base64'
 
 def csv_to_base64_gzipped_yaml(csv_file_path)
   # 1. Read the CSV file and build a hash
-  data_hash = {}
+  data_array = []
   CSV.foreach(csv_file_path, headers: false) do |row|
     key, value = row
-    data_hash[key] = value
+    data_array << [key, value]
   end
 
   # 2. Convert the hash to a YAML string
-  yaml_str = data_hash.to_yaml
+  yaml_str = data_array.to_yaml
 
   # 3. Gzip the YAML string
   compressed = Zlib::Deflate.deflate(yaml_str)
-
   # 4. Base64-encode the compressed data
   Base64.strict_encode64(compressed)
 end
+
+
 
 base64_string = csv_to_base64_gzipped_yaml(ARGV[0])
 File.open("output.txt", "w") do |file|
