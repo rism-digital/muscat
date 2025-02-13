289bb168ba74d6e09cb4df56d7e3ba0cbfc824bf
Rodolfo Zitellini <rwz@xhero.org>
2025-02-13 09:57:49 +0100

    Add option for hash

--- housekeeping/scrapbook/csv2string.rb
+++ housekeeping/scrapbook/csv2string.rb
@@ -3,12 +3,14 @@ require 'yaml'
 require 'zlib'
 require 'base64'
 
-def csv_to_base64_gzipped_yaml(csv_file_path)
-  # 1. Read the CSV file and build a hash
-  data_array = []
+def csv_to_base64_gzipped_yaml(csv_file_path, hash = true)
+  data_array = hash ? Hash.new : Array.new
+  puts "Converting #{csv_file_path} to a #{data_array.class}"
+
   CSV.foreach(csv_file_path, headers: false) do |row|
     key, value = row
-    data_array << [key, value]
+    data_array << [key, value] if !hash
+    data_array[key] = value if hash
   end
 
   # 2. Convert the hash to a YAML string
@@ -20,9 +22,12 @@ def csv_to_base64_gzipped_yaml(csv_file_path)
   Base64.strict_encode64(compressed)
 end
 
+hash = true
+if ARGV.count > 1
+  hash = false if ARGV[1].downcase == "array"
+end
 
-
-base64_string = csv_to_base64_gzipped_yaml(ARGV[0])
+base64_string = csv_to_base64_gzipped_yaml(ARGV[0], hash)
 File.open("output.txt", "w") do |file|
     file.write base64_string
 end
