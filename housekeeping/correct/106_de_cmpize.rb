bef15ccdb7491724b6e663493c60930fb09e3ca1
Rodolfo Zitellini <rwz@xhero.org>
2025-02-17 12:41:56 +0100

    Make sure repeated tags and works work

--- housekeeping/correct/106_de_cmpize.rb
+++ housekeeping/correct/106_de_cmpize.rb
@@ -3,13 +3,17 @@ def decmpize(model)
   count =  model.where("marc_source like '%$4cmp%'").count
   pb = ProgressBar.new(count)
 
+  tag = "700"
+  tag = "500" if model == Work
+
   model.where("marc_source like '%$4cmp%'").each do |s|
       save = false
 
-      s.marc.each_by_tag("700") do |t|
-          tgs = t.fetch_first_by_tag("4")
-          tgs.content = "att" if tgs&.content == "cmp"
-          save = true if tgs&.content == "att"
+      s.marc.each_by_tag(tag) do |t|
+          t.fetch_all_by_tag("4").each do |tgs|
+            tgs.content = "att" if tgs&.content == "cmp"
+            save = true if tgs&.content == "att"
+          end
         end
 
       #s.paper_trail_event = "Change 700 $4 cmp to att"
@@ -28,4 +32,4 @@ def decmpize(model)
   end
 end
 
-[Source, Holding, Institution, InventoryItem, Publication, Work, WorkNode].each {|m| decmpize(m)}
+[Source, Holding, Institution, InventoryItem, Publication, Work].each {|m| decmpize(m)}
