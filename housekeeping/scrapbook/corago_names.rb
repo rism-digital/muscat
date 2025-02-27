7832ce0b05da6a7680de7bcf07111953083a7624
Rodolfo Zitellini <rwz@xhero.org>
2025-02-27 09:04:57 +0100

    More Coragoing

--- housekeeping/scrapbook/corago_names.rb
+++ housekeeping/scrapbook/corago_names.rb
@@ -4,13 +4,14 @@ not_found = []
 
 CSV::foreach("NOMI_CORAGO_NOME.csv") do |line|
 
-    name = "#{line[0]}, #{line[1]}"
+    #name = "#{line[0]}, #{line[1]}"
+    name = [line[0]&.strip, line[1]&.strip].compact.join(', ')
 
     prs = Person.where(full_name: name)
     if prs.count > 0
         prs.each do |pr|
             #puts "#{pr.id} #{name}"
-            exact_matches[name] |= Array.new
+            exact_matches[name] ||= Array.new
             exact_matches[name] << {muscat_id: pr.id, muscat_name: pr.full_name, corago_name: name}
         end
     else
@@ -25,28 +26,51 @@ CSV::foreach("NOMI_CORAGO_NOME.csv") do |line|
         end
 
         if res.results.count > 0
-            puts name.yellow
+            #puts name.yellow
             res.results.each do |pp|
                 #puts pp.full_name.green
-                maybe_matches[name] |= Array.new
-                maybe_matches[name] << {muscat_id: pr.id, muscat_name: pr.full_name}
+                maybe_matches[name] ||= Array.new
+                maybe_matches[name] << {muscat_id: pp.id, muscat_name: pp.full_name}
             end
         else
             not_found << name
         end
-
+        sleep(0.005)
     end
 
 end
 
 sheet = RODF::Spreadsheet.new
-table = sheet.table("Exact matches")
+table_single = sheet.table("Exact matches, single")
+table_multiple = sheet.table("Exact matches, multiple")
+maybe_single = sheet.table("Maybe, single")
+maybe_multiple = sheet.table("Maybe, multiple")
 
 exact_matches.each do |name, mus|
-    row = table.row
+    row = mus.count == 1 ? table_single.row : table_multiple.row
     row.cell(name)
-    row.cell(mus.first[:muscat_id])
-    row.cell(mus.first[:muscat_name])
+
+    mus.each do |mm|
+        row.cell(mm[:muscat_id])
+        row.cell(mm[:muscat_name])
+    end
+end
+
+maybe_matches.each do |name, mus|
+    row = mus.count == 1 ? maybe_single.row : maybe_multiple.row
+    #row = table.row
+    row.cell(name)
+
+    mus.each do |mm|
+        row.cell(mm[:muscat_id])
+        row.cell(mm[:muscat_name])
+    end
+end
+
+table = sheet.table("Not found")
+not_found.each do |nf|
+    row = table.row
+    row.cell(nf)
 end
 
 sheet.write_to "corago2muscat.ods"
