c79099307e88f9fc55104181f6e03a056c0f92d3
Rodolfo Zitellini <rwz@xhero.org>
2025-06-12 11:33:47 +0200

    More EDTF: make messages meaningful, make validation work

--- app/assets/javascripts/edtf_subfield_ujs.js
+++ app/assets/javascripts/edtf_subfield_ujs.js
@@ -34,9 +34,16 @@
 		init: function(object) {
 
 			function update_edtf(obj) {
-				const parsed_date = edtf($(obj).val());
-				const formatted = edtf_format(parsed_date, 'en-US')
-				$("#edtf-error").html(formatted);
+				try {
+					const parsed_date = edtf($(obj).val());
+					//const formatted = edtf_format(parsed_date, 'en-US')
+					$("#edtf-message").html("EDTF Date parser is happy! ☺︎");
+					$("#edtf-error").empty();
+				} catch (err) {
+					const first_3_lines = err.message.split(/\r?\n/).slice(0, 4).join('\n');
+					$("#edtf-message").html("It was not possible to parse the EDTF date ☹");
+					$("#edtf-error").html($('<pre>').text(first_3_lines) );
+				}
 			}
 
 			// Atach to the keyup event
