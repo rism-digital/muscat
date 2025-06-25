c3c47648b2ff6916eac1e02c41f2cdc4d0888bff
Rodolfo Zitellini <rwz@xhero.org>
2025-06-25 08:55:45 +0200

    More mods to the GND test scrip, + cornetto fixes

--- housekeeping/correct/115_test_gnd.rb
+++ housekeeping/correct/115_test_gnd.rb
@@ -1,13 +1,40 @@
+def diffize(model, id, marc1, marc2)
+  
+    lines1 = marc1.split("\n")
+    lines2 = marc2.split("\n")
+
+    diffs = Diff::LCS.sdiff(lines1, lines2)
+
+    diffs.each do |diff|
+    case diff.action
+#    when '='
+    when '!'
+        #puts "Line #{diff.old_position + 1} changed:"
+        puts "#{model}-#{id} ORIG #{diff.old_element}"
+        puts "#{model}-#{id} NEW  #{diff.new_element}"
+    when '-'
+        # Line was removed
+        puts "#{model}-#{id} REMOVED #{diff.old_element}"
+    when '+'
+        # Line was added
+        puts "#{model}-#{id} ADDED   #{diff.new_element}"
+    end
+    end
+
+end
+
 items = GND::Interface.search_for_ids({composer: "Bach"}, 10)
 
 items.each do |i|
   marc, xml = GND::Interface.retrieve(i)
   next if !marc
 
+  marc1 = marc.to_s
+
   srw_ns = {'srw' => 'http://www.loc.gov/zing/srw/'}
   marc_ns = {'marc' => 'http://www.loc.gov/MARC21/slim'}
   
-  # THis is not parsed by the Muscat MARC class
+  # This is not parsed by the Muscat MARC class
   inner_record = xml.at_xpath('//srw:record//marc:record', srw_ns.merge(marc_ns))
 
   #ap inner_record
@@ -19,4 +46,9 @@ items.each do |i|
   if !result
     puts messages
   end
+
+  marc, xml = GND::Interface.retrieve(i)
+  marc2 = marc.to_s
+
+  diffize("gnd", i, marc1, marc2)
 end
