617ddd8f009734c1ea7af29d2c5a7f92f87676a2
Rodolfo Zitellini <rwz@xhero.org>
2025-07-07 10:14:14 +0200

    Fix #1836, enable table rendering

--- app/controllers/editor_help_controller.rb
+++ app/controllers/editor_help_controller.rb
@@ -36,7 +36,7 @@ class EditorHelpController < ApplicationController
     end
 
     if file_type
-      file_data = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(file_data).html_safe
+      file_data = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true).render(file_data, ).html_safe
       file_data += "<small style='color: gray;'>Muscat v11.6</small>".html_safe
     else
       file_data += "<small style='color: gray;'>Muscat v11.4</small>".html_safe
