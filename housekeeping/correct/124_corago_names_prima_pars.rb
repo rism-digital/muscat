a0b70693e71ee70c7c91514bd2cf70c57412eb44
Rodolfo Zitellini <rwz@xhero.org>
2025-09-16 15:37:47 +0200

    Add CORAGO ids to existing records

--- housekeeping/correct/124_corago_names_prima_pars.rb
+++ housekeeping/correct/124_corago_names_prima_pars.rb
@@ -1,10 +1,53 @@
+@mc = MarcConfigCache.get_configuration("person")
+
+@the_map = {
+  "40221058": 30031214 # Ganbuzzi Innocenzo
+}
+
 def update_record(all_records)
   # Fine the record that has the RISM ID
   # And add to that
-  rism_id = nil
-  corago_ids = all_records.map{|r| r["COD_RESP0"]; rism_id = r["ID_RISM"].to_i if r["ID_RISM"] }
+  #rism_id = nil
+  #corago_ids = all_records.map{|r| r["COD_RESP0"]; rism_id = r["ID_RISM"].to_i if r["ID_RISM"] }
 
-  ap rism_id
+  idx = all_records.index { |h| h["CHK_IDRISM"] == "x" }
+  principal_entry = all_records.delete_at(idx) if idx
+
+  id = @the_map[principal_entry["ID_RISM"].to_sym] ||
+        principal_entry["ID_RISM"].to_i
+
+  p = Person.find(id)
+
+  # Add the link to the CORAGO principal entry
+  a024 = MarcNode.new("person", "024", "", @mc.get_default_indicator("024"))
+  a024.add_at(MarcNode.new("person", "2", "CORAGO", nil), 0 )
+  a024.add_at(MarcNode.new("person", "a", principal_entry["COD_RESP"], nil), 0 )
+  a024.sort_alphabetically
+  p.marc.root.add_at(a024, p.marc.get_insert_position("024") )
+
+  # Do we have more items left? Add 400s!
+  all_records.each do |r|
+    
+    fullname = [r["COGNOME"]&.strip, r["NOME"]&.strip].compact.join(", ")
+
+    fullname = "#{fullname} #{r["PREFISSO"]&.strip}" if r["PREFISSO"]
+    
+    found = false
+    p.marc["400"].each do |t|
+      found = true if t["a"].first.content.strip.downcase == fullname.downcase
+    end
+
+    puts "Add #{fullname} to #{p.id}" if !found
+
+    a400 = MarcNode.new("person", "400", "", @mc.get_default_indicator("400"))
+    a400.add_at(MarcNode.new("person", "j", "xx", nil), 0 )
+    a400.add_at(MarcNode.new("person", "a", fullname, nil), 0 )
+    a400.sort_alphabetically
+    p.marc.root.add_at(a400, p.marc.get_insert_position("400") )
+  end
+
+  p.paper_trail_event = "Add CORAGO ID #{principal_entry["COD_RESP"]}"
+  p.save
 
 end
 
@@ -18,9 +61,10 @@ grouped = rows.group_by { |r| r["COD_RESP0"].to_s.strip }  # => { "123" => [row1
 
 # Example: iterate groups
 grouped.each do |resp_id, records|
-  puts "ID #{resp_id}: #{records.size} rows"
+  next if resp_id == "COD_RESP0" # d'oh
+  #puts "ID #{resp_id}: #{records.size} rows"
   
-  action = :update
+  action = :none
 
   # One of the records contains the action to do...
   records.each do |row|
@@ -30,6 +74,8 @@ grouped.each do |resp_id, records|
       # Or else switch to the create option
       if row["ID_RISM"] == nil
         action = :create
+      else
+        action = :update
       end
 
     elsif row["CHK_IDRISM"] == "ToMatch"
@@ -38,9 +84,9 @@ grouped.each do |resp_id, records|
   end
 
   if action == :create
-    puts "Add #{resp_id}..."
+    #puts "Add #{resp_id}..."
   elsif action == :update
-    puts "Update RISM record"
+    #puts "Update RISM record"
     update_record(records)
   else
     # skip for now
