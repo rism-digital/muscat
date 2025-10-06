936a91ddcb774f093acd8b4c812095beb7cdbed2
Ferran Jorba <Ferran.Jorba@uab.cat>
2025-10-06 08:00:13 +0200

    Normalize extraneous and punctuation characters
    Change alternative characters to standarized ones, like smart quotes vs
    apopstrophes, but also different kinds of quotes, spaces or dashes.
    
    It can be used also to autocorrect misspellings or characters that are not
    easy to type in all keyboards.
    
    Closes #1599

---
+++ lib/normalize_chars.rb
@@ -0,0 +1,71 @@
+module NormalizeChars
+  # Fix typographic and exotic variations of standard chars, that
+  # creates different and duplicated forms of the same text.
+
+  NORMALIZE_CHARS = {
+    ' ' => [ # space
+      "\t", # Unicode codepoint 9, CHARACTER TABULATION
+      ' ',  # Unicode codepoint 160, NO-BREAK SPACE
+      '­',  # Unicode codepoint 173, SOFT HYPHEN
+      '‎',  # Unicode codepoint 8206, LEFT-TO-RIGHT MARK
+      '&#13;',
+      '&nbsp;',
+    ],
+    '-' => [ # dash
+      '‐', # Unicode codepoint 8208, HYPHEN
+      '‑', # Unicode codepoint 8209, NON-BREAKING HYPHEN
+      '–', # Unicode codepoint 8211, EN DASH
+      '—', # Unicode codepoint 8212, EM DASH
+      '−', # Unicode codepoint 8722, MINUS SIGN
+    ],
+    '"' => [ # double quote
+      '“', # Unicode codepoint 8220, LEFT DOUBLE QUOTATION MARK
+      '”', # Unicode codepoint 8221, RIGHT DOUBLE QUOTATION MARK
+    ],
+    "'" => [ # apostroph or single quote
+      '`', # Unicode codepoint 96, GRAVE ACCENT
+      '´', # Unicode codepoint 180, ACUTE ACCENT
+      '‘', # Unicode codepoint 8216, LEFT SINGLE QUOTATION MARK
+      '’', # Unicode codepoint 8217, RIGHT SINGLE QUOTATION MARK
+      '′', # Unicode codepoint 8242, PRIME
+      '', # Unicode codepoint 146, PRIVATE USE TWO
+    ],
+    '«' => [ # typographic open quote
+      '<<',
+      '&lt;&lt;',
+    ],
+    '»' => [ # typographic close quote
+      '>>',
+      '&gt;&gt;',
+    ],
+    'l·l' => [ # Catalan geminate el
+      'l.l',
+      'l•l',
+      'l&#61655;l',
+    ],
+    'œ' => [ # oe ligature
+      '&oelig;',
+    ],
+  }.freeze
+
+  def normalize_chars!
+    if self.respond_to? :marc_source
+      normalized = self.marc_source
+    elsif self.respond_to? :name
+      normalized = self.name
+    elsif self.respond_to? :term
+      normalized = self.term
+    elsif self.respond_to? :title
+      normalized = self.title
+    else
+      return
+    end
+    return if normalized.nil?
+    NORMALIZE_CHARS.each do |good_char, bad_chars|
+      bad_chars.each do |bad_char|
+        normalized.gsub!(bad_char, good_char)
+      end
+    end
+    normalized
+  end
+end
