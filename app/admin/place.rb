8ff89020140a2e82e26836c4f394cd00f9a6d26d
Rodolfo Zitellini <rwz@xhero.org>
2026-01-13 15:41:31 +0100

    Restore country and district in index

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -166,8 +166,8 @@ ActiveAdmin.register Place do
     selectable_column if !is_selection_mode?
     column (I18n.t :filter_id), :id  
     column (I18n.t :filter_name), :name
-    #column (I18n.t :filter_district), :district
-    #column (I18n.t :filter_country), :country
+    column (I18n.t :filter_district), :district
+    column (I18n.t :filter_country), :country
     column (I18n.t :tgn_id), :tgn_id
     column (I18n.t :hierarchy), :hierarchy
     column (I18n.t :filter_sources), :src_count_order, sortable: :src_count_order do |element|
