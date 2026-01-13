8e9762e7c4033900a9d1208915eddb26ec5a903f
Rodolfo Zitellini <rwz@xhero.org>
2026-01-13 09:25:35 +0100

    Optimize the URL checker

--- lib/parallelizator.rb
+++ lib/parallelizator.rb
@@ -10,7 +10,8 @@ class Parallelizator
                  order: :id,
                  mode: :record,
                  backend: :processes,      # :processes or :threads
-                 on_error: nil)
+                 on_error: nil,
+                 where_sql: nil)
     @relation   = model_or_relation.respond_to?(:all) ? model_or_relation.all : model_or_relation
     @fn         = fn
     @jobs       = [jobs.to_i, 1].max
@@ -19,6 +20,7 @@ class Parallelizator
     @mode       = mode
     @backend    = backend
     @on_error   = on_error
+    @where_sql  = where_sql
   end
 
   def run
@@ -28,6 +30,34 @@ class Parallelizator
     jobs = [@jobs, total].min
     per_chunk = total / jobs
     remainder = total - (per_chunk * jobs)
+
+    # Setup our progressbar
+    # we need to calculate the full set
+    scope0 = @relation
+    scope0 = scope0.where(@where_sql) if @where_sql.present?
+
+    # Cool atomic counter!
+    bar_status = Concurrent::AtomicFixnum.new(0)
+
+    bar = ProgressBar.create(
+      total: scope0.count,
+      format: "%a %B %p%% %t (processed: %c/%C)"
+    )
+
+    stop = Concurrent::AtomicBoolean.new(false)
+    renderer = Thread.new do
+      last = 0
+      until stop.true?
+        sleep 0.2
+        cur = bar_status.value
+        if cur != last
+          bar.progress = cur
+          last = cur
+        end
+      end
+      bar.progress = bar_status.value
+      bar.finish
+    end
 
     parallel_opts =
       if @backend == :threads
@@ -51,17 +81,22 @@ class Parallelizator
         cursor = 0
 
         while cursor < limit
-          batch = @relation.order(@order).limit(@batch_size).offset(offset + cursor).to_a
+          scope = @relation
+          scope = scope.where(@where_sql) if @where_sql.present?
+
+          batch = scope.order(@order).limit(@batch_size).offset(offset + cursor).to_a
           break if batch.empty?
 
           begin
             if @mode == :batch
               @fn.call(batch)
               processed += batch.size
+              bar_status.increment(batch.size)
             else
               batch.each do |record|
                 @fn.call(record)
                 processed += 1
+                bar_status.increment(1)
               end
             end
           rescue => e
@@ -76,6 +111,9 @@ class Parallelizator
         Result.new(jobid: jobid, processed: processed, errors: errors, duration_s: (ended - started))
       end
     end
+    
+    stop.make_true
+    renderer.join
   end
 
   def self.summarize(results)
