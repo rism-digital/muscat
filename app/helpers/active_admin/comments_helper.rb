2ed2474261094e2d0af139c0cb83b4abd0becdbe
Rodolfo Zitellini <rwz@xhero.org>
2026-07-14 15:36:23 +0200

    Parse and print rich formatted comments Do we want this? or only the @mentions?

--- app/helpers/active_admin/comments_helper.rb
+++ app/helpers/active_admin/comments_helper.rb
@@ -56,7 +56,7 @@ module ActiveAdmin::CommentsHelper
         end
       end
 
-      context.div render_active_admin_comment_body(comment.body), class: "active_admin_comment_body"
+      context.div render_active_admin_comment_body(comment), class: "active_admin_comment_body"
     end
   end
 
@@ -76,7 +76,115 @@ module ActiveAdmin::CommentsHelper
     send parts.join("_")
   end
 
-  def render_active_admin_comment_body(body)
+  def render_active_admin_comment_body(comment_or_body)
+    if comment_or_body.respond_to?(:body_json_document)
+      document = comment_or_body.body_json_document
+      return render_active_admin_comment_document(document) if document.present?
+
+      return render_legacy_active_admin_comment_body(comment_or_body.body)
+    end
+
+    render_legacy_active_admin_comment_body(comment_or_body)
+  end
+
+  def render_active_admin_comment_document(document)
+    return "".html_safe if document.blank?
+
+    if document.is_a?(Array)
+      return safe_join(document.map { |node| render_active_admin_comment_node(node) })
+    end
+
+    case document["type"]
+    when "doc"
+      safe_join(Array(document["content"]).map { |node| render_active_admin_comment_node(node) })
+    else
+      render_active_admin_comment_node(document)
+    end
+  end
+
+  def render_active_admin_comment_node(node)
+    return "".html_safe unless node.is_a?(Hash)
+
+    case node["type"]
+    when "paragraph"
+      content_tag(:p, render_active_admin_comment_inline_content(node["content"]))
+    when "heading"
+      level = node.dig("attrs", "level").to_i
+      level = 1 if level < 1
+      level = 6 if level > 6
+      content_tag("h#{level}", render_active_admin_comment_inline_content(node["content"]))
+    when "blockquote"
+      content_tag(:blockquote, safe_join(Array(node["content"]).map { |child| render_active_admin_comment_node(child) }))
+    when "bulletList"
+      content_tag(:ul, safe_join(Array(node["content"]).map { |child| content_tag(:li, render_active_admin_comment_node(child)) }))
+    when "orderedList"
+      content_tag(:ol, safe_join(Array(node["content"]).map { |child| content_tag(:li, render_active_admin_comment_node(child)) }))
+    when "listItem"
+      safe_join(Array(node["content"]).map { |child| render_active_admin_comment_node(child) })
+    when "hardBreak"
+      tag.br
+    else
+      render_active_admin_comment_inline_content(node["content"])
+    end
+  end
+
+  def render_active_admin_comment_inline_content(content)
+    safe_join(Array(content).map do |node|
+      case node["type"]
+      when "text"
+        render_active_admin_comment_text_node(node)
+      when "hardBreak"
+        tag.br
+      when "mention"
+        render_active_admin_comment_mention_node(node)
+      else
+        render_active_admin_comment_inline_content(node["content"])
+      end
+    end)
+  end
+
+  def render_active_admin_comment_text_node(node)
+    html = h(node["text"].to_s)
+
+    Array(node["marks"]).reduce(html) do |acc, mark|
+      render_active_admin_comment_mark(acc, mark)
+    end
+  end
+
+  def render_active_admin_comment_mark(html, mark)
+    case mark["type"]
+    when "bold"
+      content_tag(:strong, html)
+    when "italic"
+      content_tag(:em, html)
+    when "strike"
+      content_tag(:s, html)
+    when "code"
+      content_tag(:code, html)
+    when "link"
+      href = mark.dig("attrs", "href").to_s
+      return html unless href.match?(/\A(?:https?:\/\/|mailto:|\/)/)
+
+      content_tag(:a, html, href: href, rel: "nofollow noopener noreferrer", target: "_blank")
+    else
+      html
+    end
+  end
+
+  def render_active_admin_comment_mention_node(node)
+    label = node.dig("attrs", "label").presence || node.dig("attrs", "id").to_s
+    content_tag(
+      :span,
+      "@#{h(label)}".html_safe,
+      class: "comment-mention mention-editor__mention",
+      data: {
+        mention_id: node.dig("attrs", "id"),
+        mention_label: label,
+      }
+    )
+  end
+
+  def render_legacy_active_admin_comment_body(body)
     normalized_body = body.to_s.gsub("\r\n", "\n").gsub("\r", "\n")
     paragraphs = normalized_body.split(/\n{2,}/).map(&:strip).reject(&:blank?)
     paragraphs = [""] if paragraphs.empty?
