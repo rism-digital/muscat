a48f5b25805a3239e8ffd63d6838f6e8923762ce
Rodolfo Zitellini <rwz@xhero.org>
2026-08-17 11:39:55 +0200

    Make the test user generation thing use a random pass

--- housekeeping/correct/16_create_testing_users.rb
+++ housekeeping/correct/16_create_testing_users.rb
@@ -1,17 +1,21 @@
 # encoding: UTF-8
 
+require "securerandom"
+
 #users = [
 #  {name: 'Admin', email: 'admin@rism.info', password: 'password', role: 'admin', :workgroup},
 #]
 
 role = Role.where(:name => "cataloger").take
 wg = Workgroup.where(:id => 8).take #USA
+password = "aA1#{SecureRandom.alphanumeric(17)}"
 
 (1..99).each do |n|
 
   number = str = format('%02d', n)
     
-  User.create!(:name => "Training User #{number}",  :email => "training#{number}@rism.info", :roles => [role],
-               :password => "password",  :password_confirmation => "password", :workgroups => [wg])
-
+  User.create!(:name => "Training User #{number}",  :username => "Training User #{number}", :email => "training#{number}@rism.info", :roles => [role],
+               :password => password, :password_confirmation => password, :workgroups => [wg])
 end
+
+puts "Training user password: #{password}"
