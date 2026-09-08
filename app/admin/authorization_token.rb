c728bafbecdbb6abab0c4378cbad95702b33be66
Rodolfo Zitellini <rwz@xhero.org>
2026-09-08 12:33:01 +0200

    Tidy Token controller

--- app/admin/authorization_token.rb
+++ app/admin/authorization_token.rb
@@ -1,14 +1,15 @@
 ActiveAdmin.register AuthorizationToken do
-    menu :parent => "admin_menu", :label => proc {I18n.t(:authorization_tokens)}, :if => proc{ can? :manage, AuthorizationToken }
+  menu parent: "admin_menu",
+       label: proc { I18n.t(:authorization_tokens) },
+       if: proc { can? :manage, AuthorizationToken }
 
-    permit_params [:active, :name, :token, :comment]
-    config.clear_action_items!
+  permit_params :active, :name, :token, :comment
 
+  config.clear_action_items!
 
-    filter :name_cont, :as => :string
+  filter :name_cont, as: :string
 
-    sidebar :actions, :only => :index do
-        render :partial => "activeadmin/section_sidebar_index"
-      end
-
+  sidebar :actions, only: :index do
+    render partial: "activeadmin/section_sidebar_index"
+  end
 end
